198051f248b341312c21a0ed8be72ad30c7cb845
Steffen Mueller <smueller@cpan.org>
2012-08-16 09:39:40 +0200

    More type constants

--- shared_code/srl_protocol.h
+++ shared_code/srl_protocol.h
@@ -17,8 +17,7 @@
  * HASH     |         0 0 1 0 | varint      | hash, varint=length
  * ARRAY    |         0 0 1 1 | varint      | array, varint=length 
  * BLESS    |         0 1 0 0 | TAG(STR) TAG| bless item into class indicated by TAG
- * Note: BLESSV will be in a later protocol version.
- * BLESSV   |         0 1 0 1 | varint   TAG| bless item into class indicated by varint
+ *          |         0 1 0 1 |             | *reserved*
  *          |         0 1 1 x |             | *reserved*
  * 
  *          | F 1 1 1 1 y y y |             | Miscellaneous        
@@ -28,7 +27,7 @@
  * UNDEF    |         1 1 0 0 | -           | undef
  * REGEXP   |         1 1 0 1 | TAG         | next item is a regexp 
  * FLOAT    |         1 1 1 0 | (FLOAT)     | float
- *          |         1 1 1 1 |             | *reserved*
+ * CONTINUE |         1 1 1 1 |             | INDICATES NEXT BYTE AS STATE BYTE
  * 
  * F = Flag bit to indicate if the item needs to be tracked during deserialization.
  *     The offset of the tag byte should be remembered, so that it can be referenced
@@ -54,6 +53,7 @@
  * TODO: Later versions: BLESSV
  * TODO: Later versions: list of packed ints/etc
  * TODO: FALSE?
+ * TODO: Simply choose to require 64bit for floats (doubles)?
  */
 
 /* Note: both indicating protocol version, keep in sync */
@@ -61,22 +61,38 @@
 #define SRL_MAGIC_STRING "srl\x01"
 
 /* Useful constants */
+/* See also range constants below for the header byte */
 #define SRL_ASCII_SHORT_STRING_MAX_LEN (2 << 5) /* six bits */
 #define SRL_POS_INT_MAX_SIZE           ((2 << 4) - 1) /* five bits */
 #define SRL_NEG_INT_MAX_SIZE           ((2 << 3) - 1) /* four bits */
 
-/* FIXME usefulness of the below are very unclear */
+/* FIXME usefulness of the below are a bit unclear*/
+
 /* All with F bit unset! */
-#define SRL_HDR_REF    (0b01110000) /* additionally with x-bit (where to ref to) unset! */
-#define SRL_HDR_HASH   (0b01110010)
-#define SRL_HDR_ARRAY  (0b01110011)
-#define SRL_HDR_BLESS  (0b01110100)
-#define SRL_HDR_ALIAS  (0b011111010)
-#define SRL_HDR_COPY   (0b011111011)
-#define SRL_HDR_UNDEF  (0b011111100)
-#define SRL_HDR_REGEXP (0b011111101)
-#define SRL_HDR_FLOAT  (0b011111110)
+/* _LOW and _HIGH versions refering to INCLUSIVE range boundaries */
+#define SRL_HDR_ASCII_LOW   (0b00000000)
+#define SRL_HDR_ASCII_HIGH  (0b01111111)
+#define SRL_HDR_POS_LOW     (0b10000000)
+#define SRL_HDR_POS_HIGH    (0b10111111)
+#define SRL_HDR_NEG_LOW     (0b11000000)
+#define SRL_HDR_NEG_HIGH    (0b11011111)
+
+#define SRL_HDR_REF_FWD     (0b01110000) /* ref to next item */
+#define SRL_HDR_REF         (0b01110000) /* ref with varint offset */
+#define SRL_HDR_HASH        (0b01110010)
+#define SRL_HDR_ARRAY       (0b01110011)
+#define SRL_HDR_BLESS       (0b01110100)
+
+#define SRL_HDR_STRING      (0b011111000)
+#define SRL_HDR_STRING_UTF8 (0b011111001)
+#define SRL_HDR_ALIAS       (0b011111010)
+#define SRL_HDR_COPY        (0b011111011)
+#define SRL_HDR_UNDEF       (0b011111100)
+#define SRL_HDR_REGEXP      (0b011111101)
+#define SRL_HDR_FLOAT       (0b011111110)
+
+#define SRL_HDR_CONTINUE    (0b011111111)
  
 /* TODO */
 
-#endif
+#endi
