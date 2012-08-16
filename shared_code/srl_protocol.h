3e9a9225e23289e841a8da8ffd70a35fd6dae85a
Steffen Mueller <smueller@cpan.org>
2012-08-16 16:07:09 +0200

    Update constants to follow spec

--- shared_code/srl_protocol.h
+++ shared_code/srl_protocol.h
@@ -2,32 +2,59 @@
 #define SRL_PROTOCOL_H_
 
 /*
- * Protocol version 1, see constants below docs.
+ * Sereal Protocol version 1, see constants below docs.
+ *
+ * Generally speaking, structures are serialized depth-first and each item
+ * is preceded/defined by a 1-byte control character:
  *
  * +--------+-----------------+-------------+--------------------------------------------------------------------------
  *          |Bit              | follow      | Description
- *          |7 6 5 4 3 2 1 0  | bytes       | 
- * ----------------+----------+--------------------------------------------------------------------------
- * ASCII    | F 0 x x x x x x | str         | Short ascii string, xxxxxx=length 
- * POS      | F 1 0 x x x x x | x...        | Positive varint 0 .. 2^5-1
- * NEG      | F 1 1 0 x x x x | x...        | Negative varint 2^4 .. -1
+ *          | 7 6 5 4 3 2 1 0 | bytes       | 
+ * ---------+-----------------+-------------+------------------------------------------------------------
+ *          | F 0 0 s x x x x | -           | tiny ints
+ * POS      |       0 x x x x | -           | Positive nibble   0 .. 15
+ * NEG      |       1 x x x x | -           | Negative nibble -16 .. -1
  * 
- *          | F 1 1 1 0 y y y |             | Ref/Object(ish)
- * REF      |         0 0 0 x | varint?     | ref, x=0 means to next item, x=1 means to the item indicated by varint
- * HASH     |         0 0 1 0 | varint      | hash, varint=length
- * ARRAY    |         0 0 1 1 | varint      | array, varint=length 
- * BLESS    |         0 1 0 0 | TAG(STR) TAG| bless item into class indicated by TAG
- *          |         0 1 0 1 |             | *reserved*
- *          |         0 1 1 x |             | *reserved*
+ *          | F 0 1 0 0 x x x
+ * VARINT   |           0 0 0 | varint      | varint
+ * ZIPPED   |           0 0 1 | varint      | zipped varint
  * 
- *          | F 1 1 1 1 y y y |             | Miscellaneous        
- * STRING   |         1 0 0 x | varint      | string, x= utf8 flag, varint=length
- * ALIAS    |         1 0 1 0 | varint      | alias to previous item indicated by varint
- * COPY     |         1 0 1 1 | varint      | copy item at offset
- * UNDEF    |         1 1 0 0 | -           | undef
- * REGEXP   |         1 1 0 1 | TAG         | next item is a regexp 
- * FLOAT    |         1 1 1 0 | (FLOAT)     | float
- * CONTINUE |         1 1 1 1 |             | INDICATES NEXT BYTE AS STATE BYTE
+ * RESERVED - varint indicates length to skip to if a reader does not handle the type, with 0 meaning "die".
+ *          |           0 1 0 | varint      | *reserved*
+ *          |           0 1 1 | varint      | *reserved*
+ *          |           1 0 0 | varint      | *reserved*
+ *          |           1 0 1 | varint      | *reserved*
+ *          |           1 1 0 | varint      | *reserved* 
+ *          |           1 1 1 | varint      | *reserved*
+ * 
+ * NUMLIST  | F 0 1 0 1 s y y | varint pad? | numeric array (s=0 unsigned, s=1 signed), varint=length, pad if needed for alignment
+ *                      s 0 0 |             | varint (unsigned) or zipped (signed)
+ *                      s 0 1 |             | of 16 bit
+ *                      s 1 0 |             | of 32 bit
+ *                      s 1 1 |             | of 64 bit
+ * 
+ *          | F 0 1 1 0 y y y |             | Ref/Object(ish)
+ * FWDREF   |           0 0 0 |             | scalar ref to next item
+ * REF      |           0 0 1 | varint?     | scalar ref to the item indicated by varint
+ * HASH     |           0 1 0 | varint      | hash, varint=length
+ * ARRAY    |           0 1 1 | varint      | array, varint=length 
+ * BLESS    |           1 0 0 | TAG(STR) TAG| bless item into class indicated by TAG
+ * BLESSV   |           1 0 1 | varint   TAG| bless item into class indicated by varint *provisional*
+ * WEAKEN   |           1 1 0 |             | Following item is a reference and it is weakened
+ *          |           1 1 1 |             | *reserved* FIXME what to do with this?
+ * 
+ *          | F 0 1 1 1 y y y |             | Miscellaneous        
+ * STRING   |           0 0 x | varint      | string, x= utf8 flag, varint=length
+ * ALIAS    |           0 1 0 | varint      | alias to previous item indicated by varint
+ * COPY     |           0 1 1 | varint      | copy item at offset
+ * UNDEF    |           1 0 0 | -           | undef
+ * REGEXP   |           1 0 1 | TAG         | next item is a regexp 
+ * FLOAT    |           1 1 0 | (FLOAT)     | float
+ * PAD      |           1 1 1 |             | ignored byte, used by encoder to pad if necessary
+ * 
+ * ASCII    | F 1 x x x x x x | str         | Short ascii string, x=length
+ * 
+ * 
  * 
  * F = Flag bit to indicate if the item needs to be tracked during deserialization.
  *     The offset of the tag byte should be remembered, so that it can be referenced
@@ -49,11 +76,9 @@
  * but in some cases less) indicates if there is another byte to follow, with the bytes in 
  * least significant order first.
  *
- * TODO: weakrefs?
- * TODO: Later versions: BLESSV
- * TODO: Later versions: list of packed ints/etc
+ *
  * TODO: FALSE?
- * TODO: Simply choose to require 64bit for floats (doubles)?
+ * TODO: What's with floats?
  */
 
 /* Note: both indicating protocol version, keep in sync */
@@ -70,28 +95,47 @@
 
 /* All with F bit unset! */
 /* _LOW and _HIGH versions refering to INCLUSIVE range boundaries */
-#define SRL_HDR_ASCII_LOW   ((char)0b00000000)
-#define SRL_HDR_ASCII_HIGH  ((char)0b01111111)
-#define SRL_HDR_POS_LOW     ((char)0b10000000)
-#define SRL_HDR_POS_HIGH    ((char)0b10111111)
-#define SRL_HDR_NEG_LOW     ((char)0b11000000)
-#define SRL_HDR_NEG_HIGH    ((char)0b11011111)
+#define SRL_HDR_ASCII_LOW     ((char)0b01000000)
+#define SRL_HDR_ASCII_HIGH    ((char)0b01111111)
+#define SRL_HDR_POS_LOW       ((char)0b00000000)
+#define SRL_HDR_POS_HIGH      ((char)0b00001111)
+#define SRL_HDR_NEG_LOW       ((char)0b00010000)
+#define SRL_HDR_NEG_HIGH      ((char)0b00011111)
 
-#define SRL_HDR_REF_FWD     ((char)0b01110000) /* ref to next item */
-#define SRL_HDR_REF         ((char)0b01110000) /* ref with varint offset */
-#define SRL_HDR_HASH        ((char)0b01110010)
-#define SRL_HDR_ARRAY       ((char)0b01110011)
-#define SRL_HDR_BLESS       ((char)0b01110100)
+#define SRL_HDR_VARINT        ((char)0b00100000)
+#define SRL_HDR_ZIPPED        ((char)0b00100001)
 
-#define SRL_HDR_STRING      ((char)0b011111000)
-#define SRL_HDR_STRING_UTF8 ((char)0b011111001)
-#define SRL_HDR_ALIAS       ((char)0b011111010)
-#define SRL_HDR_COPY        ((char)0b011111011)
-#define SRL_HDR_UNDEF       ((char)0b011111100)
-#define SRL_HDR_REGEXP      ((char)0b011111101)
-#define SRL_HDR_FLOAT       ((char)0b011111110)
+/* Note: Can do reserved check with a range now, but as we start using
+ *       them, might have to explicit == check later. */
+#define SRL_HDR_RESERVED_LOW  ((char)0b00100010)
+#define SRL_HDR_RESERVED_HIGH ((char)0b00100111)
 
-#define SRL_HDR_CONTINUE    ((char)0b011111111)
+#define SRL_HDR_NUMLIST_VAR_U ((char)0b00101000) /* unsigned varint numlist */
+#define SRL_HDR_NUMLIST_VAR_S ((char)0b00101100) /* signed varint numlist */
+#define SRL_HDR_NUMLIST_16_U  ((char)0b00101001) /* unsigned 16bit numlist */
+#define SRL_HDR_NUMLIST_16_S  ((char)0b00101101) /* signed 16bit numlist */
+#define SRL_HDR_NUMLIST_32_U  ((char)0b00101010) /* unsigned 32bit numlist */
+#define SRL_HDR_NUMLIST_32_S  ((char)0b00101110) /* signed 32bit numlist */
+#define SRL_HDR_NUMLIST_64_U  ((char)0b00101011) /* unsigned 64bit numlist */
+#define SRL_HDR_NUMLIST_64_S  ((char)0b00101111) /* signed 64bit numlist */
+
+#define SRL_HDR_FWDREF        ((char)0b00110000) /* ref to next item */
+#define SRL_HDR_REF           ((char)0b00110001) /* ref with varint offset */
+#define SRL_HDR_HASH          ((char)0b00110010)
+#define SRL_HDR_ARRAY         ((char)0b00110011)
+#define SRL_HDR_BLESS         ((char)0b00110100)
+#define SRL_HDR_BLESSV        ((char)0b00110101) /* provisional */
+#define SRL_HDR_WEAKEN        ((char)0b00110110)
+#define SRL_HDR_FIXME         ((char)0b00110111) /* FIXME do something with this!!! */
+
+#define SRL_HDR_STRING      ((char)0b00111000)
+#define SRL_HDR_STRING_UTF8 ((char)0b00111001)
+#define SRL_HDR_ALIAS       ((char)0b00111010)
+#define SRL_HDR_COPY        ((char)0b00111011)
+#define SRL_HDR_UNDEF       ((char)0b00111100)
+#define SRL_HDR_REGEXP      ((char)0b00111101)
+#define SRL_HDR_FLOAT       ((char)0b00111110)
+#define SRL_HDR_PAD         ((char)0b00111111)
  
 /* TODO */
 
