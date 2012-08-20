6a52666bc9cb3d85fefe0c10bd1643683bab2adb
Steffen Mueller <smueller@cpan.org>
2012-08-20 08:14:52 +0200

    More flexible benchmark

--- Sereal-Encoder/author_tools/bench.pl
+++ Sereal-Encoder/author_tools/bench.pl
@@ -8,6 +8,15 @@ use Data::Dumper qw(Dumper);
 use Data::Dumper::Limited qw(DumpLimited);
 use Storable qw(nfreeze thaw);
 use Data::MessagePack;
+use Getopt::Long qw(GetOptions);
+
+GetOptions(
+  'dump|d' => \(my $dump),
+);
+
+our %opt = @ARGV;
+
+use constant SEREAL_ONLY => 0;
 
 our $mpo = Data::MessagePack->new();
 
@@ -25,39 +34,44 @@ our $data = [
   {@str},
 ];
 
-my $json_xs  = encode_json($data);
-my $dd1      = Data::Dumper->new([$data])->Indent(0)->Dump();
-my $dd2      = Dumper($data);
-my $ddl      = DumpLimited($data);
-my $sereal   = encode_sereal($data);
-my $storable = nfreeze($data);
-my $mp       = $mpo->pack($data);
-
-
-if(@ARGV){
-    print $sereal;
-    exit;
+my ($json_xs, $dd1, $dd2, $ddl, $sereal, $storable, $mp);
+if (!SEREAL_ONLY) {
+  $json_xs  = encode_json($data);
+  $dd1      = Data::Dumper->new([$data])->Indent(0)->Dump();
+  $dd2      = Dumper($data);
+  $ddl      = DumpLimited($data);
+  $storable = nfreeze($data);
+  $mp       = $mpo->pack($data);
 }
+$sereal   = encode_sereal($data, \%opt);
+
+warn $sereal if $dump;
+
 require bytes;
-print "JSON::XS:              " . bytes::length($json_xs) . " bytes\n";
-print "Data::Dumper::Limited: " . bytes::length($ddl) . " bytes\n";
-print "Data::Dumper (1):      " . bytes::length($dd1) . " bytes\n";
-print "Data::Dumper (2):      " . bytes::length($dd2) . " bytes\n";
+if (!SEREAL_ONLY) {
+  print "JSON::XS:              " . bytes::length($json_xs) . " bytes\n";
+  print "Data::Dumper::Limited: " . bytes::length($ddl) . " bytes\n";
+  print "Data::Dumper (1):      " . bytes::length($dd1) . " bytes\n";
+  print "Data::Dumper (2):      " . bytes::length($dd2) . " bytes\n";
+  print "Storable:              " . bytes::length($storable) . " bytes\n";
+  print "Data::MessagePack:     " . bytes::length($mp) . " bytes\n";
+}
 print "Sereal::Encoder:       " . bytes::length($sereal) . " bytes\n";
-print "Storable:              " . bytes::length($storable) . " bytes\n";
-print "Data::MessagePack:     " . bytes::length($mp) . " bytes\n";
 
 our $x;
 cmpthese(
   -3,
   {
-    json_xs => '$::x = encode_json($::data);',
-    ddl => '$::x = DumpLimited($::data);',
-    dd1 => '$::x = Data::Dumper->new([$::data])->Indent(0)->Dump();',
-    dd2 => '$::x = Dumper($::data);',
-    sereal => '$::x = encode_sereal($::data);',
-    storable => '$::x = nfreeze($::data);',
-    mp => '$::x = $::mpo->pack($::data);',
+    (!SEREAL_ONLY
+      ? (
+        json_xs => '$::x = encode_json($::data);',
+        ddl => '$::x = DumpLimited($::data);',
+        dd1 => '$::x = Data::Dumper->new([$::data])->Indent(0)->Dump();',
+        dd2 => '$::x = Dumper($::data);',
+        storable => '$::x = nfreeze($::data);',
+        mp => '$::x = $::mpo->pack($::data);',
+      ) : ()),
+    sereal => '$::x = encode_sereal($::data, \%::opt);',
   }
 );
 
