1ec51bfb6003b57386ac56ce0aed41195fe5d575
Steffen Mueller <smueller@cpan.org>
2012-08-21 15:28:56 +0200

    Show stream byte positions in hobodecoder
    Man, this code is so bad. It's also likely buggy. If in doubt, trust the
    experimental ENCODER over this thing. Use with caution.

--- Sereal-Encoder/author_tools/hobodecoder.pl
+++ Sereal-Encoder/author_tools/hobodecoder.pl
@@ -19,20 +19,22 @@ open my $fh, "| od -tu1c" or die $!;
 print $fh $data;
 close $fh;
 
-print "\n";
+print "\n\nTotal length: " . length($data) . "\n\n";
 
 my $dref = \$data;
 my $indent = "";
-parse_header($dref);
+my $pos = -1;
+parse_header($dref, \$pos);
 while (defined $data and $data ne '') {
-  my $done = parse_sv($dref, $indent);
+  my $done = parse_sv($dref, \$pos, $indent);
   last if $done;
 }
 
 sub parse_header {
   my $dr = shift;
+  my $ri = shift;
   $$dr =~ s/^srl.// or die "invalid header";
-  my $len = varint($dr);
+  my $len = varint($dr, $ri);
   my $hdr = substr($$dr, 0, $len);
   if (defined $hdr and length($hdr)) {
     print "Header($len): " . join(" ", map ord, split //, $hdr) . "\n";
@@ -40,64 +42,71 @@ sub parse_header {
   else {
     print "Empty Header.\n";
   }
+  $$ri += $len + 4;
 }
 
 sub parse_sv {
-  my $dr = shift;
-  my $ind = shift;
+  my ($dr, $ri, $ind) = @_;
+
+  my $p = $$ri;
   my $t = substr($$dr, 0, 1, '');
+  $$ri++;
   my $o = ord($t);
   my $high = $o > 128;
   $o -= 128 if $high;
   if ($o == SRL_HDR_VARINT) {
-    print $ind, "VARINT: " . varint($dr) . "\n";
+    printf "%06u, %sVARINT: %u\n", $p, $ind, varint($dr, $ri);
   }
   elsif ($o <= 15) {
-    print $ind, "POS: $o\n";
+    printf "%06u, %sPOS: %u\n", $p, $ind, $o;
   }
   elsif ($o <= 31) {
     $o = 15-$o;
-    print $ind, "NEG: $o\n";
-  }
-  elsif ($o == SRL_HDR_UNDEF) {
-    print $ind, "UNDEF\n";
+    printf "%06u, %sNEG: %i\n", $p, $ind, $o;
   }
   elsif ($o > 64) {
     $o -= 64;
     my $len = $o;
+    $$ri += $len;
     my $str = substr($$dr, 0, $len, '');
-    print $ind, "ASCII($len): '$str'\n";
+    printf "%06u, %sASCII(%u): '%s'\n", $p, $ind, $len, $str;
   }
   elsif ($o == SRL_HDR_STRING || $o == SRL_HDR_STRING_UTF8) {
-    my $l = varint($dr);
-    print $ind, "STRING".($o == SRL_HDR_STRING_UTF8 ? "_UTF8" : "")."($l): '";
+    my $l = varint($dr, $ri);
     my $str = substr($$dr, 0, $l, ""); # fixme UTF8
-    print $str, "'\n";
+    printf "%06u, %sSTRING".($o == SRL_HDR_STRING_UTF8 ? "_UTF8" : "")."(%u): '%s'\n", $p, $ind, $l, $str;
+    $$ri += $l;
   }
   elsif ($o == SRL_HDR_REF) {
-    print $ind, "REF\n";
-    parse_sv($dr, $ind."  ");
+    my $whence = varint($dr, $ri);
+    printf "%06u, %sREF(%u)\n", $p, $ind, $whence;
+    if ($whence == 0) {
+      parse_sv($dr, $ri, $ind."  ");
+    }
   }
   elsif ($o == SRL_HDR_REUSE) {
-    my $len = varint($dr);
-    print $ind, "REUSE($len)\n";
+    my $len = varint($dr, $ri);
+    printf "%06u, %sREUSE(%u)\n", $p, $ind, $len;
   }
   elsif ($o == SRL_HDR_COPY) {
     my $len = varint($dr);
-    print $ind, "COPY($len)\n";
+    printf "%06u, %sCOPY(%u)\n", $p, $ind, $len;
   }
   elsif ($o == SRL_HDR_ARRAY) {
-    parse_av($dr, $ind);
+    parse_av($dr, $ri, $ind);
   }
   elsif ($o == SRL_HDR_TAIL) {
-    print $ind, "TAIL\n";
+    printf "%06u, %sTAIL\n", $p, $ind;
     return 1;
   }
+  elsif ($o == SRL_HDR_UNDEF) {
+    printf "%06u, %sUNDEF\n", $p, $ind;
+  }
   elsif ($o == SRL_HDR_WEAKEN) {
-    print $ind, "WEAKEN\n";
+    printf "%06u, %sWEAKEN\n", $p, $ind;
   }
   elsif ($o == SRL_HDR_PAD) {
-    print $ind, "PAD\n";
+    printf "%06u, %sPAD\n", $p, $ind;
   }
   else {
     die "unsupported type: $o ($t): $const_names{$o}";
@@ -106,20 +115,23 @@ sub parse_sv {
 }
 
 sub parse_av {
-  my ($dr, $ind) = @_;
-  my $len = varint($dr);
-  print $ind, "ARRAY($len)\n";
+  my ($dr, $ri, $ind) = @_;
+  my $p = $$ri;
+  my $len = varint($dr, $ri);
+  printf "%06u, %sARRAY(%u)\n", $p, $ind, $len;
+  $$ri += $len;
   $ind .= "  ";
   while (1) {
     my $t = substr($$dr, 0, 1);
     my $o = ord($t);
     if ($o == SRL_HDR_TAIL) {
-      print $ind, "TAIL\n";
+      printf "%06u, %sTAIL\n", $$ri, $ind;
       substr($$dr, 0, 1, "");
+      $$ri++;
       last;
     }
     else {
-      parse_sv($dr, $ind);
+      parse_sv($dr, $ri, $ind);
     }
   }
 }
@@ -127,14 +139,17 @@ sub parse_av {
 # super inefficient
 sub varint {
   my $dr = shift;
+  my $ri = shift;
   my $x = 0;
   my $lshift = 0;
   while (length($$dr) && ord(substr($$dr, 0, 1)) & 0x80) {
     my $c = ord(substr($$dr, 0, 1, ''));
+    ++$$ri if $ri;
     $x += ($c & 0x7F) << $lshift;
     $lshift += 7;
   }
   if (length($$dr)) {
+    ++$$ri if $ri;
     my $c = ord(substr($$dr, 0, 1, ''));
     $x += $c << $lshift;
   }
