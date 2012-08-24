85615dab6c6e77a3611aee1b4c5e6eaa45f19edd
Yves Orton <yves.orton@booking.com>
2012-08-24 09:04:23 +0200

    eliminate cross contamination during benchmark
    If a serializer calls something like SvPV(foo) the object will be
    upgraded to a PV type, which may impact how other serializers would
    serialize it.
    
    For instance message-pack and sereal, and to an extent all of the
    serializers are subtly sensitive to the real type of a scalar. Storable
    on the other hand seems to call SvPV(foo) on everything. This means
    that if mp or sereal see the strucutre after Storable has looked at it
    they will see something that is quite different than had they seen it
    before.
    
    This patch changes the benchmark so each serializer operates on their
    own private copy of the structure. The results on serialization sizes
    and speed are drammatic.

--- Sereal-Encoder/author_tools/bench.pl
+++ Sereal-Encoder/author_tools/bench.pl
@@ -23,7 +23,8 @@ our $mpo = Data::MessagePack->new();
 my @str;
 push @str, join("", map chr(65+int(rand(57))), 1..10) for 1..1000;
 
-our $data = [
+our %data;
+$data{$_}= [
   [1..10000],
   {@str},
   {@str},
@@ -32,20 +33,21 @@ our $data = [
   [map rand,1..1000],
   {@str},
   {@str},
-];
+] for qw(sereal dd1 dd2 ddl mp json_xs storable);
 
 my ($json_xs, $dd1, $dd2, $ddl, $sereal, $storable, $mp);
+# do this first before any of the other dumpers "contaminate" the iv/pv issue
+$sereal   = encode_sereal($data{sereal}, \%opt);
 if (!SEREAL_ONLY) {
-  $json_xs  = encode_json($data);
-  $dd1      = Data::Dumper->new([$data])->Indent(0)->Dump();
-  $dd2      = Dumper($data);
-  $ddl      = DumpLimited($data);
-  $storable = nfreeze($data);
-  $mp       = $mpo->pack($data);
+  $json_xs  = encode_json($data{json_xs});
+  $dd1      = Data::Dumper->new([$data{dd1}])->Indent(0)->Dump();
+  $dd2      = Dumper($data{dd2});
+  $ddl      = DumpLimited($data{ddl});
+  $mp       = $mpo->pack($data{mp});
+  $storable = nfreeze($data{storable}); # must be last
 }
-$sereal   = encode_sereal($data, \%opt);
 
-warn $sereal if $dump;
+print($sereal), exit if $dump;
 
 require bytes;
 if (!SEREAL_ONLY) {
@@ -64,14 +66,14 @@ cmpthese(
   {
     (!SEREAL_ONLY
       ? (
-        json_xs => '$::x = encode_json($::data);',
-        ddl => '$::x = DumpLimited($::data);',
-        dd1 => '$::x = Data::Dumper->new([$::data])->Indent(0)->Dump();',
-        dd2 => '$::x = Dumper($::data);',
-        storable => '$::x = nfreeze($::data);',
-        mp => '$::x = $::mpo->pack($::data);',
+        json_xs => '$::x = encode_json($::data{json_xs});',
+        ddl => '$::x = DumpLimited($::data{ddl});',
+        dd1 => '$::x = Data::Dumper->new([$::data{dd1}])->Indent(0)->Dump();',
+        dd2 => '$::x = Dumper($::data{dd2});',
+        storable => '$::x = nfreeze($::data{storable});',
+        mp => '$::x = $::mpo->pack($::data{mp});',
       ) : ()),
-    sereal => '$::x = encode_sereal($::data, \%::opt);',
+    sereal => '$::x = encode_sereal($::data{sereal}, \%::opt);',
   }
 );
 
