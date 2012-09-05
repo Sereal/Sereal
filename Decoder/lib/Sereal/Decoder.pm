043724d94a73be453f483012e81480f1375beef3
Steffen Mueller <smueller@cpan.org>
2012-09-05 17:04:10 +0200

    Import csnappy library code

--- Decoder/lib/Sereal/Decoder.pm
+++ Decoder/lib/Sereal/Decoder.pm
@@ -137,10 +137,47 @@ their gratitude.
 Copyright (C) 2012 by Steffen Mueller
 Copyright (C) 2012 by Yves Orton
 
-Excluding portions taken from Marc Lehmann's code for the JSON::XS
-module. The license for JSON::XS is the same as for this module:
+The license for the code in this distribution is the following,
+with the exceptions listed below:
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
+
+Except portions taken from Marc Lehmann's code for the JSON::XS
+module, which is licensed under the same terms as this module.
+
+Also except the code for Snappy compression library, whose license
+is reproduced below and which, to the best of our knowledge,
+is compatible with this module's license. The license for the
+enclosed Snappy code is:
+
+  Copyright 2011, Google Inc.
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+    * Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+  copyright notice, this list of conditions and the following disclaimer
+  in the documentation and/or other materials provided with the
+  distribution.
+    * Neither the name of Google Inc. nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =cut
