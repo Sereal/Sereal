a89af22248230495a86f1c3a7f39276fa53b4415
Steffen Mueller <smueller@cpan.org>
2012-09-07 09:57:48 +0200

    Pretty diagrams for benchmarks

--- shared/author_tools/bench.pl
+++ shared/author_tools/bench.pl
@@ -21,6 +21,7 @@ my (
     $small_data,
     $medium_data,
     $nobless,
+    $diagrams,
 );
 BEGIN {
     my $sereal_only = 0;
@@ -34,6 +35,7 @@ BEGIN {
         'medium'    => \$medium_data,
         'no_bless|no-bless|nobless'    => \$nobless,
         'sereal_only|sereal-only|serealonly' => \$sereal_only,
+        'diagrams'  => \$diagrams,
     );
     eval "sub SEREAL_ONLY () { $sereal_only }";
 }
@@ -76,8 +78,9 @@ print($sereal), exit if $dump;
 
 my $sereal_len= bytes::length($sereal);
 require bytes;
+my @size_datasets;
 if (!SEREAL_ONLY) {
-    for my $tuple (
+    @size_datasets = (
         (($medium_data && !$nobless) ? () : (
             ["JSON::XS",  bytes::length($json_xs)],
             ["Data::Dumper::Limited", bytes::length($ddl)],
@@ -88,15 +91,17 @@ if (!SEREAL_ONLY) {
         ["Storable", bytes::length($storable)],
         ["Sereal::Encoder",  bytes::length($sereal)],
         ["Sereal::Encoder, Snappy",  bytes::length($sereal_snappy)],
-    ) {
+    );
+    for my $tuple (@size_datasets) {
         my ($name, $size) = @$tuple;
         printf "%-40s %12d bytes %.2f%% of sereal\n", $name, $size, $size/$sereal_len *100;
     }
 }
 
 our $x;
+my ($encoder_result, $decoder_result);
 if ($encoder) {
-    cmpthese(
+    $encoder_result = cmpthese(
         $duration,
         {
             (!SEREAL_ONLY
@@ -104,10 +109,10 @@ if ($encoder) {
                     ($medium_data && !$nobless ? () : (
                         json_xs => '$::x = encode_json($::data{json_xs});',
                         ddl => '$::x = DumpLimited($::data{ddl});',
-                        mp => '$::x = $::mpo->pack($::data{mp});',
+                        msgpack => '$::x = $::mpo->pack($::data{mp});',
                     )),
-                    dd1 => '$::x = Data::Dumper->new([$::data{dd1}])->Indent(0)->Dump();',
-                    dd2 => '$::x = Dumper($::data{dd2});',
+                    dd_noindent => '$::x = Data::Dumper->new([$::data{dd1}])->Indent(0)->Dump();',
+                    dd => '$::x = Dumper($::data{dd2});',
                     storable => '$::x = nfreeze($::data{storable});',
                 ) : ()),
             sereal_func => '$::x = encode_sereal($::data{sereal_func}, \%::opt);',
@@ -118,7 +123,7 @@ if ($encoder) {
 }
 
 if ($decoder) {
-    cmpthese(
+    $decoder_result = cmpthese(
         $duration,
         {
             (!SEREAL_ONLY
@@ -126,7 +131,7 @@ if ($decoder) {
                     ($medium_data && !$nobless ? () : (
                         json_xs => '$::x = decode_json($::json_xs);',
                         undump_ddl => '$::x = Data::Undump::undump($::ddl);',
-                        mp => '$::x = $::mpo->unpack($::mp);',
+                        msgpack => '$::x = $::mpo->unpack($::mp);',
                     )),
                     eval_dd => '$::x = eval $::dd1;',
                     storable => '$::x = thaw($::storable);',
@@ -176,3 +181,87 @@ sub make_data {
         ]
     }
 }
+
+if ($diagrams) {
+    require SOOT;
+    SOOT::Init(0);
+    SOOT->import(":all");
+
+    my ($enc_data, $dec_data);
+    $enc_data = cmpthese_to_sanity($encoder_result) if $encoder_result;
+    $dec_data = cmpthese_to_sanity($decoder_result) if $decoder_result;
+
+    foreach my $dia (["Encoder performance [1/s]", $enc_data],
+                     ["Decoder performance [1/s]", $dec_data],)
+    {
+        my ($title, $d) = @$dia;
+        next if not $d;
+        $_->[0] =~ s/_/ /g, $_->[0] =~ s/sereal /sereal, / for @$d;
+        make_bar_chart(
+            substr($title, 0, 3),
+            $d,
+            {title => $title}
+        );
+    }
+
+    my %names = (
+        "JSON::XS" => 'json xs',
+        "Data::Dumper::Limited" => 'ddl',
+        "Data::MessagePack" => "msgpack",
+        "Data::Dumper (1)" => "dd noindent",
+        "Data::Dumper (2)" => "dd",
+        "Storable" => 'storable',
+        "Sereal::Encoder" => 'sereal',
+        "Sereal::Encoder, Snappy" => 'sereal, snappy',
+    );
+
+    make_bar_chart(
+        "size",
+        [
+            sort {$b->[1] <=> $a->[1]} map [ $names{$_->[0]}||die, $_->[1] ], @size_datasets
+        ],
+        {
+            title => "Encoded output sizes [bytes]",
+            color => kRed(),
+        },
+    );
+    SOOT->Run;
+}
+
+sub make_bar_chart {
+    my ($name, $data, $opts) = @_;
+    my $h = TH1D->new($name, ($opts->{title}||$name), scalar(@$data), -0.5, scalar(@$data)-0.5);
+    $h->keep;
+    $h->SetFillColor($opts->{color} || kBlue());
+    $h->SetBarOffset(0.12);
+    $h->SetBarWidth(0.74);
+    $h->SetStats(0);
+    $h->GetXaxis()->SetLabelSize(0.06);
+    $h->GetXaxis()->SetLabelOffset(0.009);
+    $h->GetYaxis()->SetTitle($opts->{title}) if defined $opts->{title};
+    $h->GetYaxis()->SetTitleSize(0.045);
+    for my $i (1..@$data) {
+        my ($label, $rate) = @{ $data->[$i-1] };
+        $h->GetXaxis()->SetBinLabel($i, $label);
+        $h->SetBinContent($i, 0+$rate);
+    }
+    my $c = TCanvas->new->keep;
+    $c->GetPad(0)->SetBottomMargin(0.175);
+    $c->GetPad(0)->SetLeftMargin(0.15);
+    $c->GetPad(0)->SetRightMargin(0.115);
+    $c->GetPad(0)->SetGrid();
+    $h->Draw("bar2");
+}
+
+sub cmpthese_to_sanity {
+    my $res = shift;
+    my @rows = map {
+        my $rate = $_->[1];
+        if (not $rate =~ s/\s*\/\s*s$//) {
+            $rate = 1/$rate;
+        }
+        [$_->[0], $rate]
+    } grep {defined $_->[0] and $_->[0] =~ /\S/} @$res;
+    return \@rows;
+}
+
