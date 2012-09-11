505cd9f3702c05358c2deac72aad2567540b06a6
Steffen Mueller <smueller@cpan.org>
2012-09-11 12:59:18 +0200

    Version bumps and change logs

--- Perl/Decoder/lib/Sereal/Decoder.pm
+++ Perl/Decoder/lib/Sereal/Decoder.pm
@@ -5,7 +5,7 @@ use warnings;
 use Carp qw/croak/;
 use XSLoader;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 use Exporter 'import';
 
 our @EXPORT_OK = qw(decode_sereal);
--- Perl/Encoder/lib/Sereal/Encoder.pm
+++ Perl/Encoder/lib/Sereal/Encoder.pm
@@ -5,7 +5,7 @@ use warnings;
 use Carp qw/croak/;
 use XSLoader;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 use Exporter 'import';
 our @EXPORT;
 BEGIN { push @EXPORT,"encode_sereal" if $0=~/-e/}
