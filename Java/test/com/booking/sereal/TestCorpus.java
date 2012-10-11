9813e6d5cb652ae706b91137f118c112f340ea5b
Chris Veenboer <chris.veenboer@booking.com>
2012-10-11 10:37:19 +0200

    Improving output

--- Java/test/com/booking/sereal/TestCorpus.java
+++ Java/test/com/booking/sereal/TestCorpus.java
@@ -19,13 +19,13 @@ import com.booking.sereal.Utils.Function;
 /**
  * To make the corpus of test files:
  * perl -I Perl/shared/t/lib/ -MSereal::TestSet -MSereal::Encoder -e'Sereal::TestSet::write_test_files("test_dir")'
- *
+ * 
  * This runs the Decoder/Encoder over every file in the supplied directory and tells you when the bytes the encoder
  * outputs do not match the bytes in the test file. The purpose is to check if roundtripping to Perl type
  * datastructures works.
- *
+ * 
  * If you pass a file as parameter it will do the same but do more detailed logging.
- *
+ * 
  */
 public class TestCorpus {
 
@@ -48,16 +48,16 @@ public class TestCorpus {
 	/**
 	 * Broken tests atm:
 	 * 00035: I don't even...
-	 * 00036: broken: unicode string that is actually latin1
+	 * 00036: broken: unicode string that is actually encodable as latin1
 	 * 00085: long unicode string that contains only latin1, so we can't roundtrip...
-	 *
+	 * 
 	 * @param args
 	 * @throws IOException
 	 * @throws SerealException
 	 */
 	public static void main(String[] args) throws IOException {
 
-		String manual = "../test_dir/test_data_00096";
+		String manual = "../test_dir/test_data_00114";
 
 		if( args.length == 0 && manual == null ) {
 			throw new UnsupportedOperationException( "Usage: Example [test_dir OR test_data_00XXXX]" );
@@ -83,19 +83,28 @@ public class TestCorpus {
 
 	}
 
+	static int ok_dec = 0;
+	static int ok_enc = 0;
+	static int ok_round = 0;
+
 	private static boolean roundtrip(File target) {
 
 		enc.reset();
 
 		try {
-			System.out.print( "Roundtrip encoding " + target.getName() );
+			System.out.print( "Testing " + target.getName() + " -" );
 
+			System.out.print( " Decode: " );
 			Object data = dec.decodeFile( target );
+			System.out.print( "OK" );
+			ok_dec++;
 			if( verbose ) {
 				System.out.println( "\nDecoding Done: " + Utils.dump( data ) + "\n" );
 			}
+			System.out.print( " Encode: " );
 			ByteBuffer encoded = enc.write( data );
-
+			System.out.print( "OK" );
+			ok_enc++;
 			if( writeEncoded ) {
 				FileOutputStream fos = new FileOutputStream( new File( target.getAbsolutePath() + "_java_encoded" ) );
 				fos.write( encoded.array() );
@@ -110,8 +119,10 @@ public class TestCorpus {
 				System.out.println( "Encoded  : " + Utils.hexStringFromByteArray( encoded.array(), 4 ) );
 				System.out.println( "\nStructure: " + sd.decodeFile( target ) );
 			}
+			System.out.print( " Roundtrip: " );
 			Assert.assertArrayEquals( "Roundtrip fail for: " + target.getName(), buf.array(), encoded.array() );
-			System.out.println( ": Success!" );
+			System.out.println( "OK" );
+			ok_round++;
 		} catch (SerealException e) {
 			e.printStackTrace( System.out );
 			return false;
@@ -122,14 +133,14 @@ public class TestCorpus {
 			if( verbose ) {
 				System.out.println( a.getMessage() );
 			} else {
-				System.out.println( ": Fail" );
+				System.out.println( "Fail" );
 			}
 			return false;
 		} catch (Exception e) {
 			if( verbose ) {
 				e.printStackTrace();
 			} else {
-				System.out.println( ": Fail" );
+				System.out.println( "Fail" );
 			}
 			return false;
 		}
@@ -138,7 +149,7 @@ public class TestCorpus {
 
 	}
 
-	protected static void decodeAllTests(final File test_dir) throws IOException {
+	protected static void decodeAllTests(final File test_dir) {
 
 		// get all files called test_data_...
 		String[] filenames = test_dir.list( new FilenameFilter() {
@@ -159,7 +170,6 @@ public class TestCorpus {
 
 		} );
 
-		int win = 0;
 		for(File test : tests) {
 
 			boolean success = roundtrip( test );
@@ -167,10 +177,11 @@ public class TestCorpus {
 				System.out.println( "Aborting after first error" );
 				return;
 			}
-			win += success ? 1 : 0;
 
 		}
-		System.out.printf( "Ratio: %d/%d = %.2f%%\n", win, tests.size(), ((double) 100 * win / tests.size()) );
+		System.out.printf( "Decoded: %d/%d = %.2f%%\n", ok_dec, tests.size(), ((double) 100 * ok_dec / tests.size()) );
+		System.out.printf( "Encoded: %d/%d = %.2f%%\n", ok_enc, tests.size(), ((double) 100 * ok_enc / tests.size()) );
+		System.out.printf( "Roundtrip: %d/%d = %.2f%%\n", ok_round, tests.size(), ((double) 100 * ok_round / tests.size()) );
 
 	}
 
