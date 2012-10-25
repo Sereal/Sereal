b9c1718e959b9f6a1a340639a0d73a275c16c810
Steffen Mueller <smueller@cpan.org>
2012-10-25 11:32:35 +0200

    Perl: Change log and version bump

--- Perl/Encoder/lib/Sereal/Encoder.pm
+++ Perl/Encoder/lib/Sereal/Encoder.pm
@@ -5,10 +5,10 @@ use warnings;
 use Carp qw/croak/;
 use XSLoader;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 # not for public consumption, just for testing.
-my $TestCompat = [qw( 0.14 0.13 0.12 0.11 0.10 0.09 0.08 0.07 0.06 )];
+my $TestCompat = [qw( 0.15 0.14 0.13 0.12 0.11 0.10 0.09 0.08 0.07 0.06 )];
 sub _test_compat {return(@$TestCompat, $VERSION)}
 
 use Exporter 'import';
