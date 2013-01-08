b2fd8392cc1312bd712dd6fb5900449dd80da4d3
Steffen Mueller <smueller@cpan.org>
2013-01-08 07:41:23 +0100

    Sereal meta module: depend on 0.23 of everything

--- Perl/Sereal/lib/Sereal.pm
+++ Perl/Sereal/lib/Sereal.pm
@@ -4,10 +4,10 @@ use strict;
 use warnings;
 our $VERSION;
 BEGIN {
-    $VERSION = '0.210';
+    $VERSION = '0.230';
 }
-use Sereal::Encoder 0.20 qw(encode_sereal);
-use Sereal::Decoder 0.21 qw(decode_sereal looks_like_sereal);
+use Sereal::Encoder 0.23 qw(encode_sereal);
+use Sereal::Decoder 0.23 qw(decode_sereal looks_like_sereal);
 
 use Exporter 'import';
 our @EXPORT_OK = qw(encode_sereal decode_sereal looks_like_sereal);
