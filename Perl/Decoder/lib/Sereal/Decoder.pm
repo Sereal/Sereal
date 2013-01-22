afac04807da2ed3ad8fb45f5116dd8af3384fd9d
Steffen Mueller <smueller@cpan.org>
2013-01-22 17:58:46 +0100

    Perl: Changelog and version bump to 0.25

--- Perl/Decoder/lib/Sereal/Decoder.pm
+++ Perl/Decoder/lib/Sereal/Decoder.pm
@@ -5,10 +5,10 @@ use warnings;
 use Carp qw/croak/;
 use XSLoader;
 
-our $VERSION = '0.24'; # Don't forget to update the TestCompat set for testing against installed encoders!
+our $VERSION = '0.25'; # Don't forget to update the TestCompat set for testing against installed encoders!
 
 # not for public consumption, just for testing.
-my $TestCompat = [map sprintf("%.2f", $_/100), reverse(23..23)]; # compat with 0.23 to ...
+my $TestCompat = [map sprintf("%.2f", $_/100), reverse(23..24)]; # compat with 0.23 to ...
 sub _test_compat {return(@$TestCompat, $VERSION)}
 
 use Exporter 'import';
