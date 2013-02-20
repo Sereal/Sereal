97aff2ed8e0183d5ec00c0d0f683d84d12081405
Damian Gryski <damian@gryski.com>
2013-02-20 09:58:19 +0100

    go: trivial round-trip tester -- needs work for nested types

---
+++ Go/sereal_test.go
@@ -0,0 +1,35 @@
+package sereal
+
+import (
+	"reflect"
+	"testing"
+)
+
+var roundtrips = []interface{}{
+	true,
+	false,
+	1,
+	10,
+	100,
+	200,
+	300,
+	"hello",
+	"hello, world",
+	"twas brillig and the slithy toves and gyre and gimble in the wabe",
+//        []interface{}{1,100,1000,2000,0xdeadbeef},
+}
+
+func TestRoundtrip(t *testing.T) {
+
+	for _, v := range roundtrips {
+		b, err := Marshal(v)
+		if err != nil {
+			t.Errorf("failed marshalling : %v\n", v)
+		}
+		var unp interface{}
+		Unmarshal(b, &unp)
+		if !reflect.DeepEqual(v, unp) {
+                    t.Errorf("failed roundtripping: %#v: got %#v\n", v, unp)
+		}
+	}
+}
