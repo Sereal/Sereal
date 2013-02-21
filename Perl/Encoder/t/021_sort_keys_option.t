Sereal/Sereal

commit 67e3d11a7239209e154c6b3b0ab4d192257f36af

Add support for a sort_keys option in the encoder
This makes it possible to compare hashes dumps using Sereal without
having hash order issues.

Note that it does not generally solve the problem of producing a
canonical representation of a data structure, it only solves the
problem of ensuring that hashes are output in a predictable order.

Perl/Encoder/srl_encoder.c
@@ -12,6 +12,8 @@ extern "C" {
 }
 #endif
 
+#include <stdlib.h>
+
 #ifndef PERL_VERSION
 #    include <patchlevel.h>
 #    if !(defined(PERL_VERSION) || (PERL_SUBVERSION > 0 && defined(PATCHLEVEL)))
@@ -252,6 +254,12 @@ srl_build_encoder_struct(pTHX_ HV *opt)
         if ( svp && SvTRUE(*svp) ) {
             undef_unknown = 1;
             enc->flags |= SRL_F_UNDEF_UNKNOWN;
+        }
+
+        svp = hv_fetchs(opt, "sort_keys", 0);
+        if ( svp && SvTRUE(*svp) ) {
+            undef_unknown = 1;
+            enc->flags |= SRL_F_SORT_KEYS;
         }
 
         svp = hv_fetchs(opt, "stringify_unknown", 0);
@@ -732,6 +740,36 @@ srl_dump_av(pTHX_ srl_encoder_t *enc, AV *src, U32 refcount)
     }
 }
 
+/* compare hash entries, used when all keys are bytestrings */
+static int
+he_cmp_fast(const void *a_, const void *b_)
+{
+    /* even though we are called as a callback from qsort there is
+     * no need for a dTHX here, we don't use anything that needs it */
+    int cmp;
+
+    HE *a = *(HE **)a_;
+    HE *b = *(HE **)b_;
+
+    STRLEN la = HeKLEN (a);
+    STRLEN lb = HeKLEN (b);
+
+    if (!(cmp = memcmp (HeKEY (b), HeKEY (a), lb < la ? lb : la)))
+        cmp = lb - la;
+
+    return cmp;
+}
+
+/* compare hash entries, used when some keys are sv's or utf8 */
+static int
+he_cmp_slow(const void *a, const void *b)
+{
+    /* we are called as a callback from qsort, so no pTHX
+     * is possible in our argument signature, so we need to do a
+     * dTHX; here ourselves. */
+    dTHX;
+    return sv_cmp( HeSVKEY_force( *(HE **)b), HeSVKEY_force( *(HE **)a ) );
+}
 
 static SRL_INLINE void
 srl_dump_hv(pTHX_ srl_encoder_t *enc, HV *src, U32 refcount)
@@ -740,7 +778,7 @@ srl_dump_hv(pTHX_ srl_encoder_t *enc, HV *src, U32 refcount)
     const int do_share_keys = HvSHAREKEYS((SV *)src);
     UV n;
 
-    if ( SvMAGICAL(src) ) {
+    if ( SvMAGICAL(src) || SRL_ENC_HAVE_OPTION(enc, SRL_F_SORT_KEYS) ) {
         UV i;
         /* for tied hashes, we have to iterate to find the number of entries. Alas... */
         (void)hv_iterinit(src); /* return value not reliable according to API docs */
@@ -760,18 +798,58 @@ srl_dump_hv(pTHX_ srl_encoder_t *enc, HV *src, U32 refcount)
 
         (void)hv_iterinit(src); /* return value not reliable according to API docs */
         i = 0;
-        while ((he = hv_iternext(src))) {
-            SV *v;
-            if (expect_false( i == n ))
+        if (SRL_ENC_HAVE_OPTION(enc, SRL_F_SORT_KEYS)) {
+            HE **he_array;
+            int fast = 1;
+            Newxz(he_array, n, HE*);
+            SAVEFREEPV(he_array);
+            while ((he = hv_iternext(src))) {
+                if (expect_false( i == n ))
+                    croak("Panic: Trying to dump a tied hash that has a different number of keys in each iteration is just daft. Do not do that.");
+                he_array[i++]= he;
+                if (HeKLEN (he) < 0 || HeKUTF8 (he))
+                    fast = 0;
+            }
+            if (expect_false( i != n ))
                 croak("Panic: Trying to dump a tied hash that has a different number of keys in each iteration is just daft. Do not do that.");
-            v= hv_iterval(src, he);
-            srl_dump_hk(aTHX_ enc, he, do_share_keys);
-            CALL_SRL_DUMP_SV(enc, v);
-            ++i;
-        }
-        if (expect_false( i != n ))
-            croak("Panic: Trying to dump a tied hash that has a different number of keys in each iteration is just daft. Do not do that.");
+            if (fast) {
+                qsort(he_array, n, sizeof (HE *), he_cmp_fast);
+            } else {
+                /* hack to forcefully disable "use bytes" */
+                COP cop= *PL_curcop;
+                cop.op_private= 0;
 
+                ENTER;
+                SAVETMPS;
+
+                SAVEVPTR (PL_curcop);
+                PL_curcop= &cop;
+
+                qsort(he_array, n, sizeof (HE *), he_cmp_slow);
+
+                FREETMPS;
+                LEAVE;
+            }
+            for ( i= 0; i < n ; i++ ) {
+                SV *v;
+                he= he_array[i];
+                v= hv_iterval(src, he);
+                srl_dump_hk(aTHX_ enc, he, do_share_keys);
+                CALL_SRL_DUMP_SV(enc, v);
+            }
+        } else {
+            while ((he = hv_iternext(src))) {
+                SV *v;
+                if (expect_false( i == n ))
+                    croak("Panic: Trying to dump a tied hash that has a different number of keys in each iteration is just daft. Do not do that.");
+                v= hv_iterval(src, he);
+                srl_dump_hk(aTHX_ enc, he, do_share_keys);
+                CALL_SRL_DUMP_SV(enc, v);
+                ++i;
+            }
+            if (expect_false( i != n ))
+                croak("Panic: Trying to dump a tied hash that has a different number of keys in each iteration is just daft. Do not do that.");
+        }
     } else {
         n= HvUSEDKEYS(src);
         /* heuristic: n = ~min size of n values;

Perl/Decoder/lib/Sereal/Decoder.pm
@@ -5,7 +5,7 @@ use warnings;
 use Carp qw/croak/;
 use XSLoader;
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 # not for public consumption, just for testing.
 my $TestCompat = [map sprintf("%.2f", $_/100), reverse(6..20)]; # compat with 0.06 to ...
@@ -292,6 +292,7 @@ it under the same terms as Perl itself.
 
 Except portions taken from Marc Lehmann's code for the JSON::XS
 module, which is licensed under the same terms as this module.
+(Many thanks to Marc for inspiration, and code.)
 
 Also except the code for Snappy compression library, whose license
 is reproduced below and which, to the best of our knowledge,

Perl/Encoder/lib/Sereal/Encoder.pm
@@ -5,7 +5,7 @@ use warnings;
 use Carp qw/croak/;
 use XSLoader;
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 # not for public consumption, just for testing.
 my $TestCompat = [map sprintf("%.2f", $_/100), reverse(6..20)]; # compat with 0.06 to ...
@@ -155,6 +155,21 @@ do so.
 
 Do note that the setting is somewhat approximate. Setting it to 10000 may break at
 somewhere between 9997 and 10003 nested structures depending on their types.
+
+=item sort_keys
+
+Normally C<Sereal::Encoder> will output hashes in whatever order is convenient,
+generally that used by perl to actually store the hash, or whatever order
+was returned by a tied hash.
+
+If this option is enabled then the Encoder will sort the keys before outputting
+them. It uses more memory, and is quite a bit slower than the default.
+
+Generally speaking this should mean that a hash and a copy should produce the
+same output. Nevertheless the user is warned that Perl has a way of "morphing"
+variables on use, and some of its rules are a little arcane (for instance utf8
+keys), and so two hashes that might appear to be the same might still produce
+different output as far as Sereal is concerned.
 
 =back
 

Perl/Encoder/srl_encoder.h
@@ -75,7 +75,11 @@ void srl_dump_data_structure(pTHX_ srl_encoder_t *enc, SV *src);
 
 /* Only meaningful if SRL_F_WARN_UNKNOWN also set. If this one is set, then we don't warn
  * if the unsupported item has string overloading. */
-#define SRL_F_NOWARN_UNKNOWN_OVERLOAD        256UL
+#define SRL_F_NOWARN_UNKNOWN_OVERLOAD       256UL
+
+/* Only meaningful if SRL_F_WARN_UNKNOWN also set. If this one is set, then we don't warn
+ * if the unsupported item has string overloading. */
+#define SRL_F_SORT_KEYS                     512UL
 
 /* Set while the encoder is in active use / dirty */
 #define SRL_OF_ENCODER_DIRTY                 1UL

Perl/Encoder/t/020_sort_keys.t
@@ -1,0 +1,67 @@
+#!perl
+use strict; 
+use warnings;
+use Sereal::Encoder qw(encode_sereal);
+use List::Util qw(shuffle); 
+use Test::More;
+
+# Try and find 15 hash collisions in "A".."Z"
+# we will use the colliding keys to produce hashes
+# with the same contents, but with different key orders,
+# which we will use to test the "sort_keys" logic.
+
+my $max= 15;
+my %hash; 
+my (%i, %j); 
+keys %i = $max; 
+keys %j = $max; 
+
+LOOP: 
+for my $x ("A" .. "Z") {
+    for my $y ( chr(ord($x)+1) .. "Z" ) {
+        %i= ();
+        %j= ();        
+        $i{$x}= 1; $i{$y}= 1; 
+        $j{$y}= 1; $j{$x}= 1; 
+        if ("@{[keys %i]}" ne "@{[keys %j]}") {  # collission?
+            $hash{$x}= 1; 
+            last LOOP if keys %hash == $max; 
+            $hash{$y}= 1; 
+            last LOOP if keys %hash == $max;
+        } 
+    }
+}
+
+my %copy= %hash;
+my $copy_keys= join "", keys %copy;
+
+my %bigger= %hash;
+keys(%bigger)= $max++ while scalar(%bigger) eq scalar(%hash);
+
+my %shuffled;
+$shuffled{$_}= $hash{$_} for shuffle keys %hash;
+
+my %encoded;
+my %encoded_unsorted;    
+for ( \%hash, \%copy, \%bigger, \%shuffled ) {
+    my $keys= join "", keys %$_;
+    $encoded{$keys} ||= encode_sereal($_, { sort_keys => 1 } );
+    $encoded_unsorted{$keys} ||= encode_sereal($_);
+}
+my @keys= keys %encoded;
+
+if ( @keys > 1 ) {
+    plan tests => 2 * ( (@keys * (@keys-1)) / 2 );
+} else {
+    plan skip_all => "Could not generate test hashes";
+}
+
+foreach my $x ( 0 .. $#keys ) {
+    foreach my $y ($x + 1 .. $#keys) {
+        is($encoded{$keys[$x]}, $encoded{$keys[$y]},"$keys[$x] vs $keys[$y] (same: sort_keys)");
+        isnt($encoded_unsorted{$keys[$x]}, $encoded_unsorted{$keys[$y]},"$keys[$x] vs $keys[$y] (different: no sort_keys)");
+    }
+}
+
+
+

Perl/shared/t/lib/Sereal/TestSet.pm
@@ -594,6 +594,7 @@ sub run_roundtrip_tests {
         ['plain',       {                  } ],
         ['snappy',      { snappy => 1      } ],
         ['snappy_incr', { snappy_incr => 1 } ],
+        ['sort_keys',   { sort_keys => 1   } ],
     ) {
         run_roundtrip_tests_internal(@$opt);
     }

commit 08c6c258ec59a3ddb9032e428128600413592a78

srl_encoder.c: fix bug when sort_keys,stringify_unknown are combined
Bug introduced in commit 67e3d11a7239209e154c6b3b0ab4d192257f36af.
Looks like a copy/paste error.

Variable undef_unknown mistakenly set to 1 when option 'sort_keys'
is true. If 'stringify_unknown' is also true then we croak with
the message
  'undef_unknown' and 'stringify_unknown' options are mutually exclusive

The fix simply doesn't assign undef_unknown=1 when 'sort_keys' is true.
A test case t/021_sort_keys_option.t is also added.

Perl/Encoder/srl_encoder.c
@@ -257,7 +257,6 @@ srl_build_encoder_struct(pTHX_ HV *opt)
 
         svp = hv_fetchs(opt, "sort_keys", 0);
         if ( svp && SvTRUE(*svp) ) {
-            undef_unknown = 1;
             enc->flags |= SRL_F_SORT_KEYS;
         }
 

Perl/Encoder/t/021_sort_keys_option.t
@@ -1,0 +1,10 @@
+#!perl
+use strict;
+use warnings;
+use Test::More tests => 1;
+use Sereal::Encoder qw(encode_sereal);
+
+eval { encode_sereal(\1, { sort_keys => 1, stringify_unknown => 1 }); };
+ok !$@, "We shouldn't die on sort_keys combined with stringify_unknown";
+
+