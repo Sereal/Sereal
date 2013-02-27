51211f699adc36bccb4fba70053f975c16c140ab
Andreas Guðmundsson <andreassonurgudmunds@gmail.com>
2013-02-27 18:37:44 -0300

    python: Delete unused macro print_description

--- Python/shared/util.h
+++ Python/shared/util.h
@@ -8,26 +8,4 @@
 /* Be aware that flag and bitmask are evaluated twice */
 #define SET_IFF(pred,flag,bitmask) ((flag) = (pred) ? (flag | (bitmask)) : (flag & ~(bitmask)))
 
-SRL_STATIC_INLINE  void print_description(PyObject *obj, char *name)
-#ifdef NDEBUG
-{}
-#else
-{
-    if (obj)
-        printf("%s: %p, %s, %s, %s, %s, %s\n"
-               "%s: %s\n", 
-               name, obj,
-               (PyNumber_Check(obj) ? "number" : "non-number"),
-               (PySequence_Check(obj) ? "sequence" : "non-sequence"),
-               (PyMapping_Check(obj) ? "mapping" : "non-mapping"),
-               (PyIter_Check(obj) ? "iterable" : "non-iterable"),
-               (PyDict_Check(obj) ? "dict" : "non-dict"),
-               name,
-               PyString_AsString(PyObject_Repr(obj))
-            );
-    else 
-        printf("%s: NULL\n", name);
-}
-#endif /* NDEBUG - print_description*/
-
 #endif
