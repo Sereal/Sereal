d33613009b19e1e3156c4eb1d975d45d1e085ff1
Aleksander Modzelewski <aleksander.modzelewski@booking.com>
2013-05-30 13:54:39 +0200

    Make Latin1String behave like a byte[] internally

--- Java/src/com/booking/sereal/Latin1String.java
+++ Java/src/com/booking/sereal/Latin1String.java
@@ -2,51 +2,52 @@ package com.booking.sereal;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 
 public class Latin1String implements CharSequence {
 
-	private final Charset charset_latin1 = Charset.forName( "ISO-8859-1" );
-	private final String s;
+	private final static Charset charset_latin1 = Charset.forName( "ISO-8859-1" );
+    private final byte[] bytes;
 
 	public Latin1String(String s) {
-		this.s = s;
+        this.bytes = s.getBytes(charset_latin1);
 	}
 
     public Latin1String(byte[] bytes) {
-        this.s = Charset.forName( "ISO-8859-1" ).decode( ByteBuffer.wrap(bytes) ).toString();
+        this.bytes = bytes;
     }
 
 	@Override
 	public String toString() {
-		return  s;
+		return Charset.forName( "ISO-8859-1" ).decode( ByteBuffer.wrap(bytes) ).toString();
 	}
 
     @Override
     public int hashCode() {
-        return s.hashCode();
+        return this.toString().hashCode();
     }
 	
 	@Override
 	public char charAt(int index) {
-		return s.charAt( index );
+		return this.toString().charAt(index);
 	}
 
 	@Override
 	public int length() {
-		return s.length();
+		return this.toString().length();
 	}
 
 	@Override
 	public CharSequence subSequence(int start, int end) {
-		return s.subSequence( start, end );
+		return this.toString().subSequence(start, end);
 	}
 
 	public byte[] getBytes() {
-		return s.getBytes( charset_latin1 );
+		return this.bytes;
 	}
 
 	public String getString() {
-		return s;
+		return this.toString();
 	}
 
     @Override
@@ -55,10 +56,9 @@ public class Latin1String implements CharSequence {
 
         if(o instanceof Latin1String) {
             Latin1String other = (Latin1String) o;
-            result = this.getString().equals(other.getString());
+            result = Arrays.equals(this.getBytes(), other.getBytes());
         }
 
         return result;
     }
-
 }
