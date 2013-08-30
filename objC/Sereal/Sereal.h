f98304c1ba44b8ec4352c7a068a2d2b79bab90c3
xant <xant@xant.net>
2013-08-30 17:47:33 +0200

    wrapper class to provide both encoding and deconding functionalities

--- objC/Sereal/Sereal.h
+++ objC/Sereal/Sereal.h
@@ -8,6 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+@class SrlEncoder;
+@class SrlDecoder;
+
 @interface Sereal : NSObject
+
+@property (assign) BOOL compress;
+@property (nonatomic, assign) BOOL decodeBinaryAsLatin1;
+@property (nonatomic, assign) NSUInteger compressionThreshold;
+
+@property (readonly) SrlEncoder *encoder;
+@property (readonly) SrlDecoder *decoder;
+@property (readonly) NSError *error;
+
+- (NSData *)encode:(id)obj;
+- (NSData *)encode:(id)data error:(NSError **)err;
+
+- (id)decode:(NSData *)obj;
+- (id)decode:(NSData *)data error:(NSError **)err;
 
 @end
--- objC/Sereal/Sereal.m
+++ objC/Sereal/Sereal.m
@@ -7,7 +7,74 @@
 //
 
 #import "Sereal.h"
+#import "SrlEncoder.h"
+#import "SrlDecoder.h"
 
 @implementation Sereal
+
+@synthesize encoder, decoder, error;
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        encoder = [[SrlEncoder alloc] init];
+        decoder = [[SrlDecoder alloc] init];
+    }
+    return self;
+}
+
+- (NSData *)encode:(id)obj
+{
+    NSData *data = [encoder encode:obj];
+    error = encoder.error;
+    return data;
+}
+
+- (id)decode:(NSData *)data
+{
+    id obj = [decoder decode:data];
+    error = decoder.error;
+    return obj;
+}
+
+- (NSData *)encode:(id)obj error:(NSError **)err
+{    
+    return [encoder encode:obj error:err];
+}
+
+- (id)decode:(NSData *)data error:(NSError **)err
+{
+    return [decoder decode:data error:err];
+}
+
+- (BOOL)compress
+{
+    return encoder.compress;
+}
+
+- (void)setCompress:(BOOL)compress
+{
+    encoder.compress = compress;
+}
+
+- (NSUInteger)compressionThreshold
+{
+    return encoder.compressionThreshold;
+}
+
+- (void)setCompressionThreshold:(NSUInteger)compressionThreshold
+{
+    encoder.compressionThreshold = compressionThreshold;
+}
+
+- (BOOL)decodeBinaryAsLatin1
+{
+    return decoder.decodeBinaryAsLatin1;
+}
+
+- (void)setDecodeBinaryAsLatin1:(BOOL)decodeBinaryAsLatin1
+{
+    decoder.decodeBinaryAsLatin1 = decodeBinaryAsLatin1;
+}
 
 @end
