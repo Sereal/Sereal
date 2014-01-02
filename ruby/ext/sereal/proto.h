16787d6df891cf170164a9091d9161483b94e6ec
borislav nikolov <jack@sofialondonmoskva.com>
2014-01-02 19:22:27 +0100

    ruby: bump sereal protocol version of the encoder

--- ruby/ext/sereal/proto.h
+++ ruby/ext/sereal/proto.h
@@ -1,7 +1,7 @@
 #define SRL_MAGIC_STRING                "=srl"          /* Magic string for header. Every packet starts with this */
 #define SRL_MAGIC_STRING_LILIPUTIAN     0x6c72733d      /* SRL_MAGIC_STRING as a little endian integer */
 
-#define SRL_PROTOCOL_VERSION            ( 1 )           /* this is the first. for some reason we did not use 0 */
+#define SRL_PROTOCOL_VERSION            ( 2 )           /* this is the first. for some reason we did not use 0 */
 #define SRL_PROTOCOL_VERSION_BITS       ( 4 )           /* how many bits we use for the version, the rest go to the encoding */
 #define SRL_PROTOCOL_VERSION_MASK       ( ( 1 << SRL_PROTOCOL_VERSION_BITS ) - 1 )
 
