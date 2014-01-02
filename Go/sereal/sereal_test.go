Sereal/Sereal

commit 6b2b15ab4f84192e02cb2c6ef16616885a860bf4

go: clean up some dead-code from cached structtags change
We now always return a map of field->index mappings for all public
fields.  This means the tags map is non-nil if there are any fields to
marshal.  This patch updates the code to make this (non-nil map) the
normal case.

Go/sereal/decode.go
@@ -420,7 +420,7 @@ func (d *Decoder) decode(b []byte, idx int, tracked map[int]reflect.Value, ptr r
 			ptr.Set(reflect.ValueOf(m))
 		}
 
-		structTags := getStructTags(ptr)
+		tags := getStructTags(ptr)
 
 		if trackme {
 			tracked[startIdx] = ptr
@@ -435,13 +435,13 @@ func (d *Decoder) decode(b []byte, idx int, tracked map[int]reflect.Value, ptr r
 			}
 
 			idx += sz
-			rval, _ := getValue(ptr, key, structTags)
+			rval, _ := getValue(ptr, key, tags)
 			sz, err = d.decode(b, idx, tracked, rval)
 			if err != nil {
 				return 0, err
 			}
 			idx += sz
-			setKeyValue(ptr, key, rval, structTags)
+			setKeyValue(ptr, key, rval, tags)
 		}
 
 	case tag == typeARRAY:
@@ -983,30 +983,26 @@ func getStructTags(ptr reflect.Value) map[string]int {
 	return nil
 }
 
-func getValue(ptr reflect.Value, key string, structTags map[string]int) (reflect.Value, bool) {
+func getValue(ptr reflect.Value, key string, tags map[string]int) (reflect.Value, bool) {
 	if ptr.Kind() == reflect.Map {
 		return reflect.New(ptr.Type().Elem()).Elem(), true
 	}
 
 	if ptr.Kind() == reflect.Struct {
 
-		if structTags != nil {
-			i, ok := structTags[key]
-			if !ok {
-				var iface interface{}
-				return reflect.ValueOf(&iface).Elem(), false
-			}
+		if tags == nil {
+			// struct has no public fields
+			var iface interface{}
+			return reflect.ValueOf(&iface).Elem(), false
+		}
+
+		if i, ok := tags[key]; ok {
 			return ptr.Field(i), true
 		}
 
-		f := ptr.FieldByName(key)
-		if f.IsValid() {
-			return f, true
-		}
-		f = ptr.FieldByName(strings.Title(key))
-		if f.IsValid() {
-			return f, true
-		}
+		// unknown field name
+		var iface interface{}
+		return reflect.ValueOf(&iface).Elem(), false
 	}
 
 	var iface interface{}
@@ -1014,7 +1010,7 @@ func getValue(ptr reflect.Value, key string, structTags map[string]int) (reflect
 	return reflect.ValueOf(&iface).Elem(), false
 }
 
-func setKeyValue(ptr reflect.Value, key string, val reflect.Value, structTags map[string]int) {
+func setKeyValue(ptr reflect.Value, key string, val reflect.Value, tags map[string]int) {
 
 	if ptr.Kind() == reflect.Map {
 		if ptr.IsNil() {
@@ -1026,26 +1022,27 @@ func setKeyValue(ptr reflect.Value, key string, val reflect.Value, structTags ma
 
 	if ptr.Kind() == reflect.Struct {
 
-		if structTags != nil {
-			i, ok := structTags[key]
-			if !ok {
-				return
-			}
+		if tags == nil {
+			// no public fields, nothing to set
+			return
+		}
+
+		// look for the key we know
+		if i, ok := tags[key]; ok {
 			f := ptr.Field(i)
 			f.Set(val)
 			return
 		}
 
-		f := ptr.FieldByName(key)
-		if !f.IsValid() {
-			f = ptr.FieldByName(strings.Title(key))
-		}
-
-		if !f.IsValid() {
+		// look for the title-cased key
+		tkey := strings.Title(key)
+		if i, ok := tags[tkey]; ok {
+			f := ptr.Field(i)
+			f.Set(val)
 			return
 		}
 
-		f.Set(val)
+		// not found
 		return
 	}
 

Go/sereal/encode.go
@@ -544,23 +544,8 @@ func (e *Encoder) encodeStruct(by []byte, st reflect.Value, strTable map[string]
 
 	by = e.encodeBytes(by, []byte(typ.Name()), strTable)
 
-	l := typ.NumField()
-	publicFields := 0
-
-	structTags := getStructTags(st)
-
-	if structTags != nil {
-		publicFields = len(structTags)
-	} else {
-
-		for i := 0; i < l; i++ {
-			fty := typ.Field(i)
-			if fty.PkgPath != "" {
-				continue // skip unexported names
-			}
-			publicFields++
-		}
-	}
+	tags := getStructTags(st)
+	publicFields := len(tags)
 
 	if e.PerlCompat {
 		// must be a reference
@@ -570,18 +555,9 @@ func (e *Encoder) encodeStruct(by []byte, st reflect.Value, strTable map[string]
 	by = append(by, typeHASH)
 	by = varint(by, uint(publicFields))
 
-	if structTags != nil {
-		for f, i := range structTags {
+	if tags != nil {
+		for f, i := range tags {
 			by = e.encodeString(by, f, strTable)
-			by, _ = e.encode(by, st.Field(i), strTable, ptrTable)
-		}
-	} else {
-		for i := 0; i < l; i++ {
-			fty := typ.Field(i)
-			if fty.PkgPath != "" {
-				continue // skip unexported names
-			}
-			by = e.encodeString(by, fty.Name, strTable)
 			by, _ = e.encode(by, st.Field(i), strTable, ptrTable)
 		}
 	}

commit 316e78cd7a752525066f5071db5698738345f331

go: fix title-casing key names on decode
The decoder allows unpacking a key 'name' into a struct field Name.
This functionality was broken in 6b2b15ab4 .  This fixes it and adds a
test to prevent it breaking again.

Go/sereal/decode.go
@@ -1000,6 +1000,11 @@ func getValue(ptr reflect.Value, key string, tags map[string]int) (reflect.Value
 			return ptr.Field(i), true
 		}
 
+		tkey := strings.Title(key)
+		if i, ok := tags[tkey]; ok {
+			return ptr.Field(i), true
+		}
+
 		// unknown field name
 		var iface interface{}
 		return reflect.ValueOf(&iface).Elem(), false

Go/sereal/sereal_test.go
@@ -308,6 +308,11 @@ func TestStructs(t *testing.T) {
 		Siblings int    // no tag, isn't unpacked
 	}
 
+	type ALowerTags struct {
+		Name  string `sereal:"name"`
+		Phone string `sereal:"phone"`
+	}
+
 	tests := []struct {
 		what     string
 		input    interface{}
@@ -341,6 +346,12 @@ func TestStructs(t *testing.T) {
 		{
 			"encode struct with tags",
 			ATags{Name: "12345", Phone: "mr foo", Siblings: 10},
+			A{},
+			A{Name: "mr foo", Phone: "12345"},
+		},
+		{
+			"decode struct with lower-case field names",
+			ALowerTags{Name: "mr foo", Phone: "12345"},
 			A{},
 			A{Name: "mr foo", Phone: "12345"},
 		},