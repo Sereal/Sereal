e3d38d25ca9e7d67f32be6e2f2d38a00e0649782
Damian Gryski <damian@gryski.com>
2014-06-03 17:07:15 +0200

    explicitly list the perl module dependencies

--- travis/perl.sh
+++ travis/perl.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-pushd Perl/Decoder; cpanm --quiet --installdeps --notest . ; perl Makefile.PL && make ; popd
-pushd Perl/Encoder; cpanm --quiet --installdeps --notest . ; perl Makefile.PL && make ; popd
+cpanm Test::LongString
+cpanm Test::Warn
+
+pushd Perl/Decoder; perl Makefile.PL && make ; popd
+pushd Perl/Encoder; perl Makefile.PL && make ; popd
 
 pushd Perl/Decoder ; make test ; popd
 pushd Perl/Encoder ; make test ; popd
