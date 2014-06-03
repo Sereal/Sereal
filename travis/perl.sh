966ab4526fd87b0c18ea14da31877e33a678ab3d
Damian Gryski <damian@gryski.com>
2014-06-03 15:41:28 +0200

    sereal decoder needs to come first

--- travis/perl.sh
+++ travis/perl.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-pushd Perl/Encoder; cpanm --quiet --installdeps --notest . ; perl Makefile.PL && make ; popd
 pushd Perl/Decoder; cpanm --quiet --installdeps --notest . ; perl Makefile.PL && make ; popd
+pushd Perl/Encoder; cpanm --quiet --installdeps --notest . ; perl Makefile.PL && make ; popd
 
 pushd Perl/Decoder ; make test ; popd
 pushd Perl/Encoder ; make test ; popd
