59f926fa4a6c3c571e1bc3174ca1384b1757fb81
Damian Gryski <damian@gryski.com>
2014-06-03 15:37:16 +0200

    ask cpanm to grab deps when building sereal modules

--- travis/perl.sh
+++ travis/perl.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-sudo apt-get install libtest-longstring-perl libtest-warn-perl
+pushd Perl/Encoder; cpanm --quiet --installdeps --notest . ; perl Makefile.PL && make ; popd
+pushd Perl/Decoder; cpanm --quiet --installdeps --notest . ; perl Makefile.PL && make ; popd
 
-pushd Perl/Encoder && perl Makefile.PL && make && popd
-pushd Perl/Decoder && perl Makefile.PL && make && popd
-
-pushd Perl/Decoder && make test ; popd
-pushd Perl/Encoder && make test ; popd
+pushd Perl/Decoder ; make test ; popd
+pushd Perl/Encoder ; make test ; popd
