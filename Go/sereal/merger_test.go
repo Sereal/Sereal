d9f0eac716a542054d0f9c1edd7cf919e1f8075d
Ivan Kruglov <ivan.kruglov@yahoo.com>
2014-07-01 21:20:22 +0200

    merger_test: simple benchmark for profiling

---
+++ Go/sereal/merger_test.go
@@ -0,0 +1,36 @@
+package sereal
+
+import (
+    "testing"
+    "io/ioutil"
+    "path/filepath"
+)
+
+func BenchmarkMerger(b *testing.B) {
+    files, _ := filepath.Glob("data/??.srl")
+    if files == nil {
+        b.Fatal("no files found")
+    }
+
+    var data [][]byte
+    for _, file := range files {
+        buf, ok := ioutil.ReadFile(file)
+        if ok != nil {
+            b.Fatal("failed to read file: " + file)
+        }
+
+        data = append(data, buf)
+    }
+
+    b.ResetTimer()
+
+    for i := 0; i < b.N; i++ {
+        var m = NewMerger()
+        for _, buf := range data {
+            err := m.Append(buf)
+            if err != nil {
+                panic(err);
+            }
+        }
+    }
+}
