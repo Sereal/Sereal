14ee03dedb2b5cce32270a98d93914a8aa61de61
Yves Orton <yves.orton@booking.com>
2014-07-27 12:23:17 +0200

    Fixup alignedness checks to do the right thing.
    I botched the alignedness checks when I tried to make
    it default to "on". This cleans up the mess. We now
    check things in the right order, and fallback to using
    aligned logic when we don't know otherwise.

--- Perl/shared/srl_common.h
+++ Perl/shared/srl_common.h
@@ -17,19 +17,24 @@
 
 #if defined(_M_IX86) || defined(_M_X64) || defined(__i386__) || defined(__i386) || defined(__i486__) || defined(__i486) || defined(i386) || defined(__ia64__) || defined(__x86_64__) || defined(__x86_64)
 /* SRL_X86_OR_X64_CPU is only used to help set the below macros. */
-#define SRL_X86_OR_X64_CPU 1
+#define SRL_X86_OR_X64_CPU
 #endif
 
-#if SRL_X86_OR_X64_CPU && !defined(SRL_USE_ALIGNED_LOADS_AND_STORES)
+#ifndef SRL_USE_ALIGNED_LOADS_AND_STORES
+
+#ifdef __hpux
+/* HP-UX runs on Itanium but has strict alignment so we check it first. */
+#define SRL_USE_ALIGNED_LOADS_AND_STORES 1
+#elif defined(SRL_X86_OR_X64_CPU)
 /* Set SRL_USE_ALIGNED_LOADS_AND_STORES to 0 on CPU's that permit efficient integer loads and stores from unaligned addresses. */
 #define SRL_USE_ALIGNED_LOADS_AND_STORES 0
-#endif
-
-/* HP-UX runs on Itanium but has strict alignment. */
-#ifdef __hpux
-#undef SRL_USE_ALIGNED_LOADS_AND_STORES
+#else
+/* When in doubt use aligned loads and stores */
 #define SRL_USE_ALIGNED_LOADS_AND_STORES 1
 #endif
+
+#endif
+
 
 /* In x86 one can try to enforce strict alignment in runtime.
  *
@@ -46,7 +51,7 @@
  * http://en.wikipedia.org/wiki/Control_register
  * http://en.wikipedia.org/wiki/FLAGS_register_(computing)
  */
-#if SRL_X86_OR_X64_CPU
+#ifdef SRL_X86_OR_X64_CPU
 #  if __x86_64__ || __x86_64
 #    define SRL_TRY_ENABLE_STRICT_ALIGN() asm("pushf\norl $0x40000, (%rsp)\npopf")
 #  elif __i386__ || __i386
