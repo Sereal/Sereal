63849acc190a586075ee78d34e4b09a88dc27e14
borislav nikolov <jack@sofialondonmoskva.com>
2014-10-11 16:05:46 +0200

    ruby: no pedantic, test for 2.0 and 2.1, check for rb_hash_clear in ruby.h

--- ruby/ext/sereal/extconf.rb
+++ ruby/ext/sereal/extconf.rb
@@ -2,7 +2,8 @@ require 'mkmf'
 
 have_func("rb_intern_str", ["ruby.h"])
 have_func("rb_sym_to_s", ["ruby.h"])
-$CFLAGS << ' -Wall -Wno-unknown-warning-option -Wno-variadic-macros -Wno-gnu-zero-variadic-macro-arguments -Wno-c99-extensions -O3 -pedantic '
+have_func("rb_hash_clear", ["ruby.h"])
+$CFLAGS << ' -Wall -Wno-unknown-warning-option -Wno-variadic-macros -Wno-gnu-zero-variadic-macro-arguments -Wno-c99-extensions -O3 '
 extension_name = 'sereal'
 dir_config(extension_name)
 create_makefile(extension_name)
