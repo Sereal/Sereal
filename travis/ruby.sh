1a783d776c0f81c6cf3f2b87b1ea046efe4f1ea8
borislav nikolov <jack@sofialondonmoskva.com>
2014-10-11 16:40:50 +0200

    travis: ruby: use rvm alias

--- travis/ruby.sh
+++ travis/ruby.sh
@@ -2,7 +2,7 @@
 
 set -e
 for ver in "1.9.3" "2.0.0" "2.1.0"; do
-    rvm use $ver
+    rvm alias create default $ver
     gem install rake-compiler
     cd ruby
     rake clean && rake && rake test
