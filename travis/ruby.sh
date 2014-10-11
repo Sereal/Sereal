ac7049979843b1d0fedf4fc6656e378196b6928b
borislav nikolov <jack@sofialondonmoskva.com>
2014-10-11 16:45:42 +0200

    travis: ruby: oops, cd ruby only once

--- travis/ruby.sh
+++ travis/ruby.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -e
+cd ruby
 for ver in "1.9.3" "2.0.0" "2.1.0"; do
     rvm alias create default $ver
     gem install rake-compiler
-    cd ruby
     rake clean && rake && rake test
 done
