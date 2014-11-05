e3fded552ff964ab3cbf58883741916cc25762d5
Ivan Kruglov <ivan.kruglov@booking.com>
2014-11-05 21:03:16 +0100

    Sereal::Merger: update Merger.pm

--- Perl/Merger/lib/Sereal/Merger.pm
+++ Perl/Merger/lib/Sereal/Merger.pm
@@ -1,17 +1,17 @@
 package Sereal::Merger;
-
-=head1 NAME
-
-Sereal::Merger - 
-
-=cut
-
+use 5.008;
 use strict;
 use warnings;
-
-our $VERSION     = '0.001';
-
+use Carp qw/croak/;
 use XSLoader;
+
+our $VERSION    = '0.001';
+our $XS_VERSION = $VERSION; $VERSION= eval $VERSION;
+
+# not for public consumption, just for testing.
+(my $num_version = $VERSION) =~ s/_//;
+my $TestCompat = [ map sprintf("%.2f", $_/100), reverse( 300 .. int($num_version * 100) ) ]; # compat with 3.00 to ...
+sub _test_compat {return(@$TestCompat, $VERSION)}
 
 XSLoader::load(__PACKAGE__, $Sereal::Merger::VERSION);
 
