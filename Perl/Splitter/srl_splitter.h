d2d9f3ef4065a15bc59ddf468d382feb9ea398c1
dams <dams@zarb.org>
2014-11-05 21:20:06 +0100

    remove useless define

--- Perl/Splitter/srl_splitter.h
+++ Perl/Splitter/srl_splitter.h
@@ -69,9 +69,4 @@ srl_splitter_t * srl_build_splitter_struct(pTHX_ HV *opt);
 void srl_destroy_splitter(pTHX_ srl_splitter_t *splitter);
 SV* srl_splitter_next_chunk(pTHX_ srl_splitter_t * splitter);
 
-/* Define what top level tag will be used. Default is SRL_F_TOPLEVEL_KEY_ARRAY */
-#define SRL_F_TOPLEVEL_KEY_SCALAR               0x00001UL
-#define SRL_F_TOPLEVEL_KEY_ARRAY                0x00002UL
-#define SRL_F_TOPLEVEL_KEY_HASH                 0x00004UL
-
 #endif
