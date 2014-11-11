bf60c00762bc4b784319644a907456d8e156b186
Yves Orton <yves.orton@booking.com>
2014-11-12 00:35:18 +0100

    faster varints?

--- Perl/Decoder/srl_decoder.c
+++ Perl/Decoder/srl_decoder.c
@@ -653,7 +653,7 @@ srl_read_header(pTHX_ srl_decoder_t *dec, SV *header_user_data)
     UV header_len;
     IV proto_version_and_encoding_flags_int= srl_validate_header_version_pv_len(aTHX_ (char *)BUF_POS(dec), BUF_SPACE(dec));
 
-    if ( proto_version_and_encoding_flags_int < 1 ) {
+    if ( expect_false(proto_version_and_encoding_flags_int < 1) ) {
         if (proto_version_and_encoding_flags_int == 0)
             SRL_ERROR("Bad Sereal header: It seems your document was accidentally UTF-8 encoded");
         else
@@ -810,23 +810,52 @@ srl_read_varint_uv_safe(pTHX_ srl_decoder_t *dec)
     if (expect_true( BUF_NOT_DONE(dec) )) {
         uv |= ((UV)*dec->pos++ << lshift);
     } else {
-        SRL_ERROR("varint terminated prematurely");
+        SRL_ERROR("end of packet reached before varint parsed");
     }
     return uv;
 }
 
-#define SET_UV_FROM_VARINT(uv, from) STMT_START {       \
-    if (*from < 0x80) {                                 \
-        uv= (UV)*from++;                                \
-    } else {                                            \
-        unsigned int lshift= 7;                         \
-        uv= (UV)(*from++ & 0x7f);                       \
-        while (*from & 0x80){                           \
-            uv |= ((UV)(*from++ & 0x7F) << lshift);     \
-            lshift += 7;                                \
-        }                                               \
-        uv |= ((UV)(*from++) << lshift);                \
-    }                                                   \
+#define SET_UV_FROM_VARINT(uv, ptr) STMT_START {       \
+    U32 b;                                                                  \
+                                                                            \
+    /* Splitting into 32-bit pieces gives better performance on 32-bit      \
+       processors. */                                                       \
+    U32 part0 = 0, part1 = 0, part2 = 0;                                    \
+    do { \
+                                                                            \
+    b = *(ptr++); part0  = b      ; if (!(b & 0x80)) break;             \
+    part0 -= 0x80;                                                          \
+    b = *(ptr++); part0 += b <<  7; if (!(b & 0x80)) break;             \
+    part0 -= 0x80 << 7;                                                     \
+    b = *(ptr++); part0 += b << 14; if (!(b & 0x80)) break;             \
+    part0 -= 0x80 << 14;                                                    \
+    b = *(ptr++); part0 += b << 21; if (!(b & 0x80)) break;             \
+    part0 -= 0x80 << 21;                                                    \
+                                                                            \
+    b = *(ptr++); part1  = b      ; if (!(b & 0x80)) break;             \
+    part1 -= 0x80;                                                          \
+    b = *(ptr++); part1 += b <<  7; if (!(b & 0x80)) break;             \
+    part1 -= 0x80 << 7;                                                     \
+    b = *(ptr++); part1 += b << 14; if (!(b & 0x80)) break;             \
+    part1 -= 0x80 << 14;                                                    \
+    b = *(ptr++); part1 += b << 21; if (!(b & 0x80)) break;             \
+    part1 -= 0x80 << 21;                                                    \
+                                                                            \
+    b = *(ptr++); part2  = b      ; if (!(b & 0x80)) break;             \
+    part2 -= 0x80;                                                          \
+    b = *(ptr++); part2 += b <<  7; if (!(b & 0x80)) break;             \
+    /* "part2 -= 0x80 << 7" is irrelevant because (0x80 << 7) << 56 is 0. */\
+                                                                            \
+    /* We have overrun the maximum size of a varint (10 bytes).  The data   \
+        must be corrupt. */                                                 \
+    SRL_ERROR("varint not terminated in time, corrupt packet");             \
+                                                                            \
+    } while (0);                                                            \
+                                                                            \
+    uv= (((UV)part0)      ) |                                               \
+        (((UV)part1) << 28) |                                               \
+        (((UV)part2) << 56);                                                \
+                                                                            \
 } STMT_END
 
 SRL_STATIC_INLINE UV
@@ -846,12 +875,58 @@ srl_read_varint_uv_nocheck(pTHX_ srl_decoder_t *dec)
 }
 
 SRL_STATIC_INLINE UV
+srl_read_varint_u32_nocheck(pTHX_ srl_decoder_t *dec)
+{
+    const U8* ptr = dec->pos;
+    U32 b;
+
+    U32 part0 = 0;
+
+    b = *(ptr++); part0  = b      ; if (!(b & 0x80)) goto done;
+    part0 -= 0x80;
+    b = *(ptr++); part0 += b <<  7; if (!(b & 0x80)) goto done;
+    part0 -= 0x80 << 7;
+    b = *(ptr++); part0 += b << 14; if (!(b & 0x80)) goto done;
+    part0 -= 0x80 << 14;
+    b = *(ptr++); part0 += b << 21; if (!(b & 0x80)) goto done;
+    part0 -= 0x80 << 21;
+    b = *(ptr++); part0 += b << 28; if (b < 16) goto done;
+
+    SRL_ERROR("varint overflows U32, cannot restore packet");
+
+   done:
+    dec->pos= (U8*)ptr;
+
+    return part0;
+}
+
+SRL_STATIC_INLINE UV
+srl_read_varint_u64_nocheck(pTHX_ srl_decoder_t *dec)
+{
+    UV uv;
+    const U8* ptr = dec->pos;
+
+    SET_UV_FROM_VARINT(uv, ptr);
+
+    dec->pos= (U8*)ptr;
+
+    return uv;
+}
+
+
+
+SRL_STATIC_INLINE UV
 srl_read_varint_uv(pTHX_ srl_decoder_t *dec)
 {
-    if (expect_true( dec->buf_end - dec->pos > 10 ))
-        return srl_read_varint_uv_nocheck(aTHX_ dec);
-    else
+    if (expect_true( dec->buf_end - dec->pos >= 10 ) || (dec->buf_end[-1] & 0x80)) {
+        if (sizeof(UV)==sizeof(U32)) {
+            return srl_read_varint_u32_nocheck(aTHX_ dec);
+        } else {
+            return srl_read_varint_u64_nocheck(aTHX_ dec);
+        }
+    } else {
         return srl_read_varint_uv_safe(aTHX_ dec);
+    }
 }
 
 SRL_STATIC_INLINE UV
