7d792a2bfa8c0f9564dac49d6b2d4ae56c7b9af5
dams <dams@zarb.org>
2014-11-26 15:13:29 +0100

    add Splitter to travis

--- travis/perl.sh
+++ travis/perl.sh
@@ -7,6 +7,8 @@ cpanm Test::Warn
 
 pushd Perl/Decoder; perl Makefile.PL && make ; popd
 pushd Perl/Encoder; perl Makefile.PL && make ; popd
+pushd Perl/Splitter; perl Makefile.PL && make ; popd
 
 pushd Perl/Decoder ; make test ; popd
 pushd Perl/Encoder ; make test ; popd
+pushd Perl/Splitter ; make test ; popd
