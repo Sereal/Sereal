62c70cb6aa2a03cb87decb3e5fdf1a5e6538ffdf
Yves Orton <yves.orton@booking.com>
2014-12-26 05:48:52 +0100

    Release v3.003_003 - Test for weakref, and build changes

--- Perl/Sereal/lib/Sereal.pm
+++ Perl/Sereal/lib/Sereal.pm
@@ -2,10 +2,10 @@ package Sereal;
 use 5.008;
 use strict;
 use warnings;
-our $VERSION = '3.003_002';
+our $VERSION = '3.003_003';
 our $XS_VERSION = $VERSION; $VERSION= eval $VERSION;
-use Sereal::Encoder 3.003_002 qw(encode_sereal sereal_encode_with_object);
-use Sereal::Decoder 3.003_002 qw(decode_sereal looks_like_sereal sereal_decode_with_object);
+use Sereal::Encoder 3.003_003 qw(encode_sereal sereal_encode_with_object);
+use Sereal::Decoder 3.003_003 qw(decode_sereal looks_like_sereal sereal_decode_with_object);
 
 use Exporter 'import';
 our @EXPORT_OK = qw(
