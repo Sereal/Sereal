0af5a03dd8b7357ce6461e13d095495b3ed41a20
Ivan Kruglov <ivan.kruglov@yahoo.com>
2015-05-09 16:00:04 +0200

    Sereal::Path: update documentation

--- Perl/Path/lib/Sereal/Path.pm
+++ Perl/Path/lib/Sereal/Path.pm
@@ -44,162 +44,76 @@ sub values {
 
 1;
 
-# use 5.008;
-# use strict qw(vars refs);
-
-# our $AUTHORITY = 'cpan:IKRUGLOV';
-# our $VERSION   = '0.1';
-
-# use Carp;
-# use Scalar::Util qw[blessed];
-# use Sereal::Path::Iterator;
-
-# sub new {
-    # defined $_[1] or die "first argument must be defined";
-
-    # my $iter;
-    # if (blessed($_[1]) && $_[1]->isa(Sereal::Path::Iterator)) {
-        # $iter = $_[1];
-    # } else {
-        # $iter = Sereal::Path::Iterator->new($_[1]);
-    # }
-
-    # bless {
-        # iter       => $iter,
-        # obj        => undef,
-        # resultType => 'VALUE',
-        # result     => [],
-    # }, $_[0];
-# }
-
-# sub _normalize {
-    # my ($self, $x) = @_;
-    # #$x =~ s/[\['](\??\(.*?\))[\]']/_callback_01($self,$1)/eg;
-    # $x =~ s/'?\.'?|\['?/;/g;
-    # $x =~ s/;;;|;;/;..;/g;
-    # $x =~ s/;\$|'?\]|'$//g;
-    # #$x =~ s/#([0-9]+)/_callback_02($self,$1)/eg;
-    # #$self->{'result'} = [];   # result array was temporarily used as a buffer
-    # return $x;
-# }
-
-# sub _store {
-    # my ($self, $path, $value) = @_;
-    # push @{ $self->{'result'} }, ( $self->{'resultType'} eq "PATH"
-                                   # ? $self->asPath($path)
-                                   # : $value ) if $path;
-    # return !!$path;
-# }
-
-# sub traverse {
-    # my ($self, $expr) = @_;
-
-    # my $norm = $self->_normalize($expr);
-    # $norm =~ s/^\$;//;
-
-    # $self->{iter}->reset;
-    # #warn("norm=$norm");
-    # return $self->_trace_next_object($norm, '$');
-# }
-
-# sub _trace_next_object {
-    # my ($self, $expr, $path) = @_;
-    # my $iter = $self->{iter};
-
-    # #warn("_trace_next_object: expr=$expr path=$path");
-
-    # return if $iter->eof;
-    # return $self->_store($path, $iter->decode) if "$expr" eq '';
-    
-    # my ($loc, $x);
-    # {
-        # my @x = split /\;/, $expr;
-        # $loc  = shift @x;
-        # $x    = join ';', @x;
-    # }
-
-    # my ($type, $cnt) = $iter->info;
-    # #warn("_trace_next_object: type=$type cnt=$cnt loc=$loc odepth=" . $iter->stack_depth);
-
-    # if ($type eq 'ARRAY') {
-        # if ($loc =~ /^\-?[0-9]+$/) {
-            # #warn("_trace_next_object: ARRAY loc=$loc");
-            # $iter->step_in;
-            # $iter->array_goto($loc);
-            # return $self->_trace_next_object($x, "$path;$loc");
-        # } elsif ($loc eq '*') {
-            # $iter->step_in;
-            # my $depth = $iter->stack_depth;
-            # #warn("_trace_next_object: WALK ARRAY, depth=$depth");
-
-            # foreach (1 .. $cnt) {
-                # $depth == $iter->stack_depth or die "assert depth inside walking array failed";
-                # #warn("_trace_next_object: WALK ARRAY offset=" . $iter->offset . " iter=$_ depth=$depth");
-
-                # $self->_trace_next_object($x, $path);
-
-                # #warn(sprintf('$iter->stack_depth > $depth (%s > %s)', $iter->stack_depth, $depth));
-                # if ($iter->stack_depth > $depth) {
-                    # #warn("_trace_next_object: WALK ARRAY srl_next_at_depth($depth)");
-                    # $iter->srl_next_at_depth($depth)
-                # }
-            # }
-
-            # $depth == $iter->stack_depth and die "assert depth after walking failed";
-        # } elsif ($loc =~ m/\,/) {
-            # $iter->step_in;
-            # my $depth = $iter->stack_depth;
-            # my @idxs = map { $_ >= 0 ? $_ : $cnt + $_ }
-                       # grep { /^\-?[0-9]+$/ }
-                       # split(/\,/, $loc);
-
-            # # TODO verify that array is sorted
-            # foreach my $idx (@idxs) {
-                # $iter->array_goto($idx);
-                # $self->_trace_next_object($x, "$path;$idx");
-                # $iter->srl_next_at_depth($depth) if $iter->stack_depth > $depth;
-            # }
-        # }
-    # } elsif ($type eq 'HASH') {
-        # if ($loc eq '*') {
-        # } elsif ($loc =~ m/\,/) {
-            # $iter->step_in;
-            # my $depth = $iter->stack_depth;
-            # my @names = grep { $_ } split(/\,/, $loc);
-
-            # foreach my $name (@names) {
-                # $self->_trace_next_object($x, "$path;$name")
-                    # if $iter->hash_exists($name);
-                # $self->step_out();
-            # }
-        # } else {
-            # $iter->step_in;
-            # #warn("_trace_next_object: HASH depth=" . $iter->stack_depth);
-
-            # if ($iter->hash_exists($loc)) {
-                # #warn("_trace_next_object: HASH key found=$loc depth=" . $iter->stack_depth);
-                # return $self->_trace_next_object($x, "$path;$loc");
-            # }
-
-            # #warn("_trace_next_object: HASH key not found=$loc depth=" . $iter->stack_depth . " offset=" . $iter->offset);
-        # }
-    # }
-
-    # ##warn("_trace_next_object: end of function depth=" . $iter->stack_depth);
-# }
-
-# 1;
-
 __END__
+
+=encoding utf8
+
+=head1 NAME
+
+Sereal::Path - XPath or JSONPath for Sereal
+
+=head1 SYNOPSIS
+
+  use Sereal::Path;
+  use Sereal::Encoder qw/encode_sereal/;
+  
+  my $data = [ { foo => 'bar' }, { foo => 'barbar' } ];
+  my $sp = Sereal::Path->new(encode_sereal($data));
+  
+  # $result will contain { foo => 'bar' }
+  my $result = sp->traverse('$[0]');
+
+  # $result1 will contain [ 'bar', 'barbar' ]
+  my $result1 = $sp->traverse('$[*][foo]');
+
+=head1 DESCRIPTION
+
+Sereal::Path for Sereal is the same as XPath for XML or JSONPath for JSON.
+Sereal::Path can directly work with encoded Sereal document. In other words,
+there is no need to decode a document before executing queries against it.
+
+As of first public release of Sereal::Path, it's considered to be in beta phase
+and is not ready for production usage.
+
+Sereal::Path derived it query syntax from JSONPath. Its documentation can be
+found at L<http://goessner.net/articles/JsonPath/>.
+
+However, Sereal::Path supports a subset of JSONPath. Below is the comparasion
+between XPath, JSONPath and Sereal::Path:
+
+  XPath           JSONPath            Sereal::Path            Description
+  /               $                   $                       The root object/element
+  .               @                   not impl                The current object/element
+  /               . or []             . or []                 Child operator
+  ..              n/a                 n/a                     Parent operator
+  //              ..                  not impl                Recursive descent. JSONPath borrows this syntax from E4X.
+  *               *                   *                       Wildcard. All objects/elements regardless their names.
+  @               n/a                 n/a                     Attribute access. JSON structures don't have attributes.
+  []              []                  []                      Subscript operator. XPath uses it to iterate over element
+                                                              collections and for predicates. In Javascript and JSON it
+                                                              is the native array operator.
+  |               [,]                 [,]                     Union operator in XPath results in a combination of node sets.
+                                                              JSONPath allows alternate names or array indices as a set.
+  n/a             [start:end:step]    not impl                Array slice operator borrowed from ES4.
+  []              ?()                 not impl                Applies a filter (script) expression.
+  n/a             ()                  not impl                Script expression, using the underlying script engine.
+  ()              n/a                 n/a                     Grouping in Xpath
+
+Items which are marked as 'not impl' will be implemented at later stages of the project.
+
+=head1 ACKNOWLEDGMENT
+
+This module was inspired by JSON::Path module by Toby Inkster which is a port
+of the PHP JSONPath implementation (version 0.8.x) by Stefan Goessner.
+See L<http://code.google.com/p/jsonpath/>.
+
+This module was originally developed for Booking.com.  With approval from
+Booking.com, this module was generalized and published on CPAN, for which the
+authors would like to express their gratitude.
 
 =head1 AUTHOR
 
 Ivan Kruglov <ivan.kruglov@yahoo.com>
-
-This module is pretty much a straight line-by-line port of the 
-JSON::Path module by Toby Inkster which is a port of the PHP JSONPath
-implementation (version 0.8.x) by Stefan Goessner. See
-L<http://code.google.com/p/jsonpath/>.
 
 =head1 COPYRIGHT AND LICENCE
 
@@ -207,7 +121,7 @@ Copyright 2007 Stefan Goessner.
 
 Copyright 2010-2013 Toby Inkster.
 
-Copyright 2014 Ivan Kruglov.
+Copyright 2014-2015 Ivan Kruglov.
 
 This module is tri-licensed. It is available under the X11 (a.k.a. MIT)
 licence; you can also redistribute it and/or modify it under the same
