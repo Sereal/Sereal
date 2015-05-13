c7319c5ceb2daf3ea238cf6602b0989a2e11d514
Ivan Kruglov <ivan.kruglov@yahoo.com>
2015-05-13 11:42:43 +0200

    Release Sereal::Path v0.003 - C89 compatibility fixes

--- Perl/Path/lib/Sereal/Path.pm
+++ Perl/Path/lib/Sereal/Path.pm
@@ -5,7 +5,7 @@ use warnings;
 use Carp qw/croak/;
 use XSLoader;
 
-our $VERSION    = '0.002';
+our $VERSION    = '0.003';
 our $XS_VERSION = $VERSION; $VERSION= eval $VERSION;
 
 XSLoader::load(__PACKAGE__, $Sereal::Path::VERSION);
