ef468cc7e7cd6fea930279f3ec21cba4df81e0c8
Damian Gryski <damian@gryski.com>
2015-07-29 10:26:40 +0200

    go: update fuzzer to ignore compressed packets
    This was mostly finding out-of-memory issues from snappy and zlib.

--- Go/sereal/fuzz.go
+++ Go/sereal/fuzz.go
@@ -4,8 +4,34 @@ package sereal
 
 func Fuzz(data []byte) int {
 	var m interface{}
+
+	header, err := readHeader(data)
+	if err != nil {
+		return 0
+	}
+
+	if headerSize+header.suffixSize > len(data) || bodyStart < 0 {
+		return 0
+	}
+
+	switch header.version {
+	case 1, 2, 3:
+		break
+	default:
+		return 0
+	}
+
+	switch header.doctype {
+	case serealRaw:
+		break
+	case serealSnappy, serealSnappyIncremental, serealZlib:
+		// ignore compressed data
+		return 0
+	}
+
 	if err := Unmarshal(data, &m); err != nil {
 		return 0
 	}
+
 	return 1
 }
