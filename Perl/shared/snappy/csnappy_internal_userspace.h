Sereal/Sereal

commit 11a20ffa85a1f73bc85158c7eedf1e85fd1fccb0

Perl: Update Snappy
Small portability fix. Fix issue with very large documents.
(32bit overflow issue)

Perl/shared/snappy/csnappy_internal_userspace.h
@@ -117,7 +117,7 @@ Albert Lee
 #define __LITTLE_ENDIAN	1234
 #define __BYTE_ORDER	LITTLE_ENDIAN
 
-#elif defined(__GLIBC__) || defined(__ANDROID__) || defined(__CYGWIN__)
+#elif defined(__GNUC__) || defined(__ANDROID__) || defined(__CYGWIN__)
 
 #include <endian.h>
 #include <byteswap.h>

Perl/shared/snappy/csnappy_decompress.c
@@ -266,7 +266,7 @@ SAW__AppendFastPath(struct SnappyArrayWriter *this,
 		    const char *ip, uint32_t len)
 {
 	char *op = this->op;
-	const int space_left = this->op_limit - op;
+	const uint32_t space_left = this->op_limit - op;
 	if (likely(space_left >= 16)) {
 		UnalignedCopy64(ip, op);
 		UnalignedCopy64(ip + 8, op + 8);
@@ -284,7 +284,7 @@ SAW__Append(struct SnappyArrayWriter *this,
 	    const char *ip, uint32_t len)
 {
 	char *op = this->op;
-	const int space_left = this->op_limit - op;
+	const uint32_t space_left = this->op_limit - op;
         if (unlikely(space_left < (int32_t)len))
 		return CSNAPPY_E_OUTPUT_OVERRUN;
 	memcpy(op, ip, len);
@@ -297,7 +297,7 @@ SAW__AppendFromSelf(struct SnappyArrayWriter *this,
 		    uint32_t offset, uint32_t len)
 {
 	char *op = this->op;
-	const int space_left = this->op_limit - op;
+	const uint32_t space_left = this->op_limit - op;
 	/* -1u catches offset==0 */
 	if (op - this->base <= offset - 1u)
 		return CSNAPPY_E_DATA_MALFORMED;

commit d9f72d52a3b5c865663f74cc4c4d87250c8fdcd2

Portability fix for Mac OS X
It appears that on Yosemite, __GNUC__ is defined, but not __GLIBC__.
Therefore 11a20ffa broke the build by making the __GNUC__ block
attempted to compile in csnappy_internal_userspace.h ; while the
right one was the __APPLE__ block, obviously. The solution is
to reorder the #define tests from the most specific to the least
specific.

Perl/shared/snappy/csnappy_internal_userspace.h
@@ -117,11 +117,6 @@ Albert Lee
 #define __LITTLE_ENDIAN	1234
 #define __BYTE_ORDER	LITTLE_ENDIAN
 
-#elif defined(__GNUC__) || defined(__ANDROID__) || defined(__CYGWIN__)
-
-#include <endian.h>
-#include <byteswap.h>
-
 #elif defined(__APPLE__)
 
 #include <machine/endian.h>
@@ -132,6 +127,11 @@ Albert Lee
 #define __BYTE_ORDER BYTE_ORDER
 #define __LITTLE_ENDIAN LITTLE_ENDIAN
 #define __BIG_ENDIAN BIG_ENDIAN
+
+#elif defined(__GNUC__) || defined(__ANDROID__) || defined(__CYGWIN__)
+
+#include <endian.h>
+#include <byteswap.h>
 
 #elif defined(__FreeBSD__) || defined(__DragonFly__) || defined(__NetBSD__)
 