d5b1655e044e0d782c71f2b2ee70bb3c60202df1
Yves Orton <demerphq@gmail.com>
2015-11-01 19:30:47 +0100

    bump version to 3.005_002
    Test release with SvPVNV/SvPVIV improvments

--- Perl/Decoder/lib/Sereal/Decoder/Constants.pm
+++ Perl/Decoder/lib/Sereal/Decoder/Constants.pm
@@ -4,7 +4,7 @@ use warnings;
 require Exporter;
 our @ISA= qw(Exporter);
 
-our $VERSION = '3.005_001'; # Don't forget to update the TestCompat set for testing against installed encoders!
+our $VERSION = '3.005_002'; # Don't forget to update the TestCompat set for testing against installed encoders!
 
 our (@EXPORT_OK, %DEFINE, %TAG_INFO_HASH, @TAG_INFO_ARRAY);
 
