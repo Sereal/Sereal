6fba12234ea9fc1adbe5f930b321f2fe70f3509d
Yves Orton <demerphq@gmail.com>
2015-11-13 19:35:11 +0100

    change error message format to be a bit easier on the eye

--- Perl/shared/srl_reader_error.h
+++ Perl/shared/srl_reader_error.h
@@ -3,14 +3,16 @@
 
 #include "srl_taginfo.h"
 
-#define SRL_RDR_BASE_ERROR_FORMAT  "Sereal: Error in %s:%u and char %"UVuf" of input: "
-#define SRL_RDR_BASE_ERROR_ARGS(buf) __FILE__, __LINE__, (UV) (1 + (buf)->pos - (buf)->start)
+#define SRL_RDR_BASE_ERROR_FORMAT_START  "Sereal: Error: "
+#define SRL_RDR_BASE_ERROR_FORMAT_END    " at offset %"UVuf" of input at %s line %u"
+#define SRL_RDR_BASE_ERROR_FORMAT(whatever) SRL_RDR_BASE_ERROR_FORMAT_START whatever SRL_RDR_BASE_ERROR_FORMAT_END
+#define SRL_RDR_BASE_ERROR_ARGS(buf)  (UV)(1 + (buf)->pos - (buf)->start), __FILE__, __LINE__
 
-#define SRL_RDR_ERROR(buf, msg)                              croak(SRL_RDR_BASE_ERROR_FORMAT "%s", SRL_RDR_BASE_ERROR_ARGS((buf)), (msg))
-#define SRL_RDR_ERRORf1(buf, fmt, var)                       croak(SRL_RDR_BASE_ERROR_FORMAT fmt,  SRL_RDR_BASE_ERROR_ARGS((buf)), (var))
-#define SRL_RDR_ERRORf2(buf, fmt, var1, var2)                croak(SRL_RDR_BASE_ERROR_FORMAT fmt,  SRL_RDR_BASE_ERROR_ARGS((buf)), (var1), (var2))
-#define SRL_RDR_ERRORf3(buf, fmt, var1, var2, var3)          croak(SRL_RDR_BASE_ERROR_FORMAT fmt,  SRL_RDR_BASE_ERROR_ARGS((buf)), (var1), (var2), (var3))
-#define SRL_RDR_ERRORf4(buf, fmt, var1, var2, var3, var4)    croak(SRL_RDR_BASE_ERROR_FORMAT fmt,  SRL_RDR_BASE_ERROR_ARGS((buf)), (var1), (var2), (var3), (var4))
+#define SRL_RDR_ERROR(buf, msg)                              croak(SRL_RDR_BASE_ERROR_FORMAT("%s"),  (msg), SRL_RDR_BASE_ERROR_ARGS((buf)))
+#define SRL_RDR_ERRORf1(buf, fmt, var)                       croak(SRL_RDR_BASE_ERROR_FORMAT(fmt),  (var), SRL_RDR_BASE_ERROR_ARGS((buf)))
+#define SRL_RDR_ERRORf2(buf, fmt, var1, var2)                croak(SRL_RDR_BASE_ERROR_FORMAT(fmt),  (var1), (var2), SRL_RDR_BASE_ERROR_ARGS((buf)))
+#define SRL_RDR_ERRORf3(buf, fmt, var1, var2, var3)          croak(SRL_RDR_BASE_ERROR_FORMAT(fmt),  (var1), (var2), (var3), SRL_RDR_BASE_ERROR_ARGS((buf)))
+#define SRL_RDR_ERRORf4(buf, fmt, var1, var2, var3, var4)    croak(SRL_RDR_BASE_ERROR_FORMAT(fmt),  (var1), (var2), (var3), (var4), SRL_RDR_BASE_ERROR_ARGS((buf)))
 
 #define SRL_RDR_ERROR_UNIMPLEMENTED(buf, tag, str)           SRL_RDR_ERRORf3((buf), "Tag %u (0x%x) '%s' is unimplemented", (tag), (tag), (str))
 #define SRL_RDR_ERROR_UNEXPECTED(buf, tag, msg)              SRL_RDR_ERRORf2((buf), "Unexpected tag SRL_HDR_%s while expecting %s", SRL_TAG_NAME((tag)), (msg))
