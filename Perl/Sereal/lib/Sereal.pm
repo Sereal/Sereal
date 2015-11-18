b787b152b8a30ad6ea717705716611e2e3fcfd88
Yves Orton <demerphq@gmail.com>
2015-11-18 19:49:15 +0100

    Release v3.006_004 - fix issue in encoder with threaded shared objects

--- Perl/Sereal/lib/Sereal.pm
+++ Perl/Sereal/lib/Sereal.pm
@@ -2,10 +2,10 @@ package Sereal;
 use 5.008;
 use strict;
 use warnings;
-our $VERSION = '3.006_003';
+our $VERSION = '3.006_004';
 our $XS_VERSION = $VERSION; $VERSION= eval $VERSION;
-use Sereal::Encoder 3.006_003 qw(encode_sereal sereal_encode_with_object);
-use Sereal::Decoder 3.006_003 qw(
+use Sereal::Encoder 3.006_004 qw(encode_sereal sereal_encode_with_object);
+use Sereal::Decoder 3.006_004 qw(
     decode_sereal looks_like_sereal decode_sereal_with_header_data
     scalar_looks_like_sereal
     sereal_decode_with_object sereal_decode_with_header_with_object
