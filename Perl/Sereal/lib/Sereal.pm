29f60afc44dad6361d9f4f1cc8840236d006dc83
Yves Orton <demerphq@gmail.com>
2015-12-07 01:06:55 +0100

    Release v3.014 - fixes for 5.8.9

--- Perl/Sereal/lib/Sereal.pm
+++ Perl/Sereal/lib/Sereal.pm
@@ -2,10 +2,10 @@ package Sereal;
 use 5.008;
 use strict;
 use warnings;
-our $VERSION = '3.012';
+our $VERSION = '3.014';
 our $XS_VERSION = $VERSION; $VERSION= eval $VERSION;
-use Sereal::Encoder 3.012 qw(encode_sereal sereal_encode_with_object);
-use Sereal::Decoder 3.012 qw(
+use Sereal::Encoder 3.014 qw(encode_sereal sereal_encode_with_object);
+use Sereal::Decoder 3.014 qw(
     decode_sereal looks_like_sereal decode_sereal_with_header_data
     scalar_looks_like_sereal
     sereal_decode_with_object sereal_decode_with_header_with_object
