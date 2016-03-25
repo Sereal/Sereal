8038623d16d5d355649fd4c1e32c7b7499a06f67
Damian Gryski <dgryski@gmail.com>
2016-03-25 14:45:09 +0100

    go: pool zlib writers
    Updates #124

--- Go/sereal/zlib_purego.go
+++ Go/sereal/zlib_purego.go
@@ -5,18 +5,33 @@ package sereal
 import (
 	"bytes"
 	"compress/zlib"
+	"sync"
 )
+
+var zlibWriterPools = make(map[int]*sync.Pool)
+
+func init() {
+	// -1 => 9
+	for i := zlib.DefaultCompression; i <= zlib.BestCompression; i++ {
+		zlibWriterPools[i] = &sync.Pool{
+			New: func() interface{} {
+				zw, _ := zlib.NewWriterLevel(nil, i)
+				return zw
+			},
+		}
+	}
+
+}
 
 func zlibEncode(buf []byte, level int) ([]byte, error) {
 
 	var comp bytes.Buffer
 
-	zw, err := zlib.NewWriterLevel(&comp, level)
-	if err != nil {
-		return nil, err
-	}
+	zw := zlibWriterPools[level].Get().(*zlib.Writer)
+	defer zlibWriterPools[level].Put(zw)
+	zw.Reset(&comp)
 
-	_, err = zw.Write(buf)
+	_, err := zw.Write(buf)
 	if err != nil {
 		return nil, err
 	}
