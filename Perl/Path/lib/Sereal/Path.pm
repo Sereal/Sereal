cbf0cd123a30bdcdc7986dc09eb15b47130d7c9b
Ivan Kruglov <ivan.kruglov@yahoo.com>
2017-02-22 22:09:26 +0100

    Release Sereal::Path v4.001_002 - fixed compilation issues

--- Perl/Path/lib/Sereal/Path.pm
+++ Perl/Path/lib/Sereal/Path.pm
@@ -6,7 +6,7 @@ use Carp qw/croak/;
 use XSLoader;
 use Sereal::Path::Iterator;
 
-our $VERSION    = '4.001_001';
+our $VERSION = '4.001_002';
 our $XS_VERSION = $VERSION; $VERSION= eval $VERSION;
 
 XSLoader::load(__PACKAGE__, $XS_VERSION);
