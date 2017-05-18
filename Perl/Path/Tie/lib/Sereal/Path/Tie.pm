ee895a17aed62c4ea8e2d843764c88e81f721fe4
Ivan Kruglov <ivan.kruglov@yahoo.com>
2017-05-18 20:09:13 +0200

    Release Sereal::Path v4.001_004 - bug fixes

--- Perl/Path/Tie/lib/Sereal/Path/Tie.pm
+++ Perl/Path/Tie/lib/Sereal/Path/Tie.pm
@@ -6,7 +6,7 @@ use Carp qw/croak/;
 use XSLoader;
 use Sereal::Path::Iterator;
 
-our $VERSION = '4.001_003';
+our $VERSION = '4.001_004';
 our $XS_VERSION = $VERSION; $VERSION= eval $VERSION;
 
 XSLoader::load(__PACKAGE__, $XS_VERSION);
