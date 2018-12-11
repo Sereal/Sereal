fa14615e8d9cac6e87005891fd1725f5b262fc37
Mattia Barbon <mattia.barbon@booking.com>
2018-12-11 10:47:01 +0100

    go: reformat using 'go fmt'

--- Go/sereal/cmd/fuzzer/main.go
+++ Go/sereal/cmd/fuzzer/main.go
@@ -12,20 +12,20 @@ func main() {
 
 	srlHeader, _ := hex.DecodeString("3d73726c0100")
 
-        var decoder sereal.Decoder
-        decoder.PerlCompat = true
+	var decoder sereal.Decoder
+	decoder.PerlCompat = true
 
 	for {
-            l := len(srlHeader) + mrand.Intn(200)
-            b := make([]byte, l)
-            crand.Read(b)
-            doc := make([]byte, l + len(srlHeader))
-            copy(doc, srlHeader)
-            copy(doc[6:], b)
-            fmt.Println(hex.Dump(doc))
-            var m interface{}
-            err := decoder.Unmarshal(doc, &m)
-            fmt.Println("err=", err)
+		l := len(srlHeader) + mrand.Intn(200)
+		b := make([]byte, l)
+		crand.Read(b)
+		doc := make([]byte, l+len(srlHeader))
+		copy(doc, srlHeader)
+		copy(doc[6:], b)
+		fmt.Println(hex.Dump(doc))
+		var m interface{}
+		err := decoder.Unmarshal(doc, &m)
+		fmt.Println("err=", err)
 	}
 
 }
