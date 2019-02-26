0ba228f18f0ae74e44086da6f1bc544a3f3e40e7
Mattia Barbon <mattia.barbon@booking.com>
2019-02-26 19:14:06 +0100

    Java: test header decoding with a document generated by Perl

--- Java/test/com/booking/sereal/HeaderTest.java
+++ Java/test/com/booking/sereal/HeaderTest.java
@@ -4,9 +4,13 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Map;
 import org.junit.Test;
 
 public class HeaderTest {
+	private static final byte[] realSerealDocument = new byte[] {
+		0x3d, (byte) 0xf3, 0x72, 0x6c, 0x04, 0x0a, 0x01, 0x51, 0x66, 0x68, 0x65, 0x61, 0x64, 0x65, 0x72, 0x02, 0x51, 0x64, 0x62, 0x6f, 0x64, 0x79, 0x01,
+	};
 	private static final String largeString = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
 	private Encoder encoder = new Encoder();
@@ -39,5 +43,21 @@ public class HeaderTest {
 		assertEquals(203, decoder.headerSize());
 		assertEquals(largeString, decoder.decodeHeader());
 		assertEquals(1L, decoder.decode());
+	}
+
+	@Test
+	public void withActualSereal() throws SerealException {
+		decoder.setData(realSerealDocument);
+
+		assertTrue(decoder.hasHeader());
+		assertEquals(9, decoder.headerSize());
+
+		@SuppressWarnings("unchecked")
+		Map header = (Map) decoder.decodeHeader(), body = (Map) decoder.decode();
+		assertEquals(2L, header.get("header"));
+		assertEquals(1L, body.get("body"));
+
+		assertTrue(decoder.hasHeader());
+		assertEquals(9, decoder.headerSize());
 	}
 }
