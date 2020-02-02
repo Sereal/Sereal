7f8227e1cdc2ead434f42abc71eac6f93cf120cb
Yves Orton <demerphq@gmail.com>
2020-02-02 08:07:04 +0100

    tweaks to perl build utils, make_all, clean_make_all, update_zstd
    make_all: updated
        make sure we rebuild the bundled libraries otherwise we could miss build issues
        change the 'git clean -dfx' to 'git clean -dfX' which makes it
        usable without cleaning files, which can be very annoying.
    clean_make_all: added
        does a git clean -dfx before calling make_all
    update_zstd: added
        copies zstd files from a zstd repo located in the same directory as
        the Sereal.git repo using relative paths.

--- Perl/fixver.pl
+++ Perl/fixver.pl
@@ -45,13 +45,12 @@ foreach my $file (@files) {
 
 print <<"EOF_TEXT";
 
-./make_all &&
+./clean_make_all &&
 git commit -a -m'Release v$to - $reason' &&
 git tag Sereal-Decoder-$to -m'Release Sereal::Decoder version $to ($reason)' &&
 git tag Sereal-Encoder-$to -m'Release Sereal::Encoder version $to ($reason)' &&
 git tag Sereal-$to -m'Release Sereal version $to ($reason)' &&
-git push && git push --tags
-
+git push && git push --tags &&
 cpan-upload-http -verbose Encoder/Sereal-Encoder-$to.tar.gz Decoder/Sereal-Decoder-$to.tar.gz Sereal/Sereal-$to.tar.gz
 
 EOF_TEXT
