d802cbfed8f44134792c57248a1db275d7326552
Yves Orton <demerphq@gmail.com>
2020-02-02 04:21:53 +0100

    Release v4.009_001 - update csnappy to the latest version to fix OpenBSD-6.4 build issues

--- Perl/Sereal/lib/Sereal.pm
+++ Perl/Sereal/lib/Sereal.pm
@@ -2,9 +2,9 @@ package Sereal;
 use 5.008;
 use strict;
 use warnings;
-our $VERSION = '4.009';
+our $VERSION = '4.009_001';
 our $XS_VERSION = $VERSION; $VERSION= eval $VERSION;
-use Sereal::Encoder 4.009 qw(
+use Sereal::Encoder 4.009_001 qw(
     encode_sereal
     sereal_encode_with_object
     SRL_UNCOMPRESSED
@@ -12,7 +12,7 @@ use Sereal::Encoder 4.009 qw(
     SRL_ZLIB
     SRL_ZSTD
 );
-use Sereal::Decoder 4.009 qw(
+use Sereal::Decoder 4.009_001 qw(
     decode_sereal
     looks_like_sereal
     decode_sereal_with_header_data
