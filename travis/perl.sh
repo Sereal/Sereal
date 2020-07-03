851f003c1ace254d43d0301766916e5f4987847a
Yves Orton <demerphq@gmail.com>
2020-07-03 15:28:31 +0200

    fixup travis perl build

--- travis/perl.sh
+++ travis/perl.sh
@@ -7,6 +7,7 @@ cpanm Test::Differences
 cpanm Test::Exception
 cpanm Test::LongString
 cpanm Test::Warn
+cpanm Test::MemoryGrowth
 
 pushd Perl/Decoder; perl Makefile.PL && make ; popd
 pushd Perl/Encoder; perl Makefile.PL && make ; popd
@@ -14,11 +15,11 @@ pushd Perl/Encoder; perl Makefile.PL && make ; popd
 pushd Perl/Decoder ; make test ; popd
 pushd Perl/Encoder ; make test ; popd
 
-cpanm Sereal::Decoder
-cpanm Sereal::Encoder
+#cpanm Sereal::Decoder
+#cpanm Sereal::Encoder
 
-pushd Perl/Splitter; perl Makefile.PL && make ; popd
-pushd Perl/Splitter; make test ; popd
+#pushd Perl/Splitter; perl Makefile.PL && make ; popd
+#pushd Perl/Splitter; make test ; popd
 
-pushd Perl/Path; perl Makefile.PL && make ; popd
-pushd Perl/Path; make test ; popd
+#pushd Perl/Path; perl Makefile.PL && make ; popd
+#pushd Perl/Path; make test ; popd
